148cbf8d91e3ebfc5f98f5b7de9eef9627fb3068
Roan Kattouw <catrope@users.mediawiki.org>
2011-12-12 17:20:15 +0000

    Fix indentation in messages array

--- ArticleFeedbackv5.hooks.php
+++ ArticleFeedbackv5.hooks.php
@@ -146,7 +146,7 @@ class ArticleFeedbackv5Hooks {
 				'articlefeedbackv5-error-flagging',
 				'articlefeedbackv5-invalid-feedback-id',
 				'articlefeedbackv5-invalid-feedback-flag',
-					'articlefeedbackv5-abuse-saved',
+				'articlefeedbackv5-abuse-saved',
 				'articlefeedbackv5-hide-saved',
 			),
 		),
