66e7543570b379c21cce173b989a1b89bced0d41
emsmith <emsmith@localhost>
2012-02-21 21:08:13 +0000

    bug 34090 - kill the -1 issue by never letting it get below 0

--- api/ApiArticleFeedbackv5Utils.php
+++ api/ApiArticleFeedbackv5Utils.php
@@ -184,7 +184,7 @@ class ApiArticleFeedbackv5Utils {
 		);
 
 		foreach ( $filters as $filter => $direction) {
-			$value = ($direction > 0) ? 'afc_filter_count + 1' : 'afc_filter_count - 1';
+			$value = ($direction > 0) ? 'afc_filter_count + 1' : 'GREATEST(0, afc_filter_count - 1)';
 
 			# Update each row with the new count.
 			$dbw->update(
