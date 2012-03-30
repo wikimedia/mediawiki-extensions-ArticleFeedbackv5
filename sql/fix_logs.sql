ba33b1ef5aa46a252278faaf0903ca4d54ca11cc
emsmith <elizabeth@omniti.com>
2012-03-30 18:26:07 -0400

    Fix the log count updater script
    Change-Id: Ic09e07b1e99aeea44ebf2441c8f0d093d8fb2dff

--- sql/fix_logs.sql
+++ sql/fix_logs.sql
@@ -7,7 +7,7 @@ UPDATE aft_article_feedback f1
    SET f1.af_activity_count = COALESCE((SELECT COUNT(*) FROM logging l2
                                     WHERE log_type = 'articlefeedbackv5'
                                       AND l2.log_title REGEXP CONCAT('/', p3.page_title, '/', f1.af_id, '$')
-                                      GROUP BY l2.log_title);
+                                      GROUP BY l2.log_title), 0);
 
 UPDATE aft_article_feedback f1
    INNER JOIN page p3 ON f1.af_page_id=p3.page_id
