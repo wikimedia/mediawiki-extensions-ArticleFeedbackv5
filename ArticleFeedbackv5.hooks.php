a6fffc3ed7a48977c2a7a504bb32fd3d2a7a47b0
emsmith <elizabeth@omniti.com>
2012-04-17 12:19:44 -0400

    Squashed commit for 4/19 deployment
    - Stage 3 clicktracking
    - Updated submit button clicktracking to have an attempt and a success or error
    - Added Option 4
    - Reinstated auto-flag as abuse custom action for AbuseFilter
    - Fixed the counts
    - Redesign of status lines; keep track of last status change
    
    Change-Id: Ie3db5fcaa24af5fa141828de14762bde1762b64e

--- ArticleFeedbackv5.hooks.php
+++ ArticleFeedbackv5.hooks.php
@@ -113,6 +113,10 @@ class ArticleFeedbackv5Hooks {
 				'articlefeedbackv5-bucket4-learn-to-edit',
 				'articlefeedbackv5-bucket4-form-submit',
 				'articlefeedbackv5-bucket4-help-tooltip-info',
+				'articlefeedbackv5-bucket4-noedit-title',
+				'articlefeedbackv5-bucket4-noedit-teaser-line1',
+				'articlefeedbackv5-bucket4-noedit-teaser-line2',
+				'articlefeedbackv5-bucket4-noedit-form-submit',
 				'articlefeedbackv5-bucket5-form-switch-label',
 				'articlefeedbackv5-bucket5-form-panel-title',
 				'articlefeedbackv5-bucket5-form-panel-explanation',
@@ -183,8 +187,6 @@ class ArticleFeedbackv5Hooks {
 				'articlefeedbackv5-form-undelete',
 				'articlefeedbackv5-form-oversight',
 				'articlefeedbackv5-form-unoversight',
-				'articlefeedbackv5-hidden',
-				'articlefeedbackv5-deleted',
 				'articlefeedbackv5-comment-more',
 				'articlefeedbackv5-comment-less',
 				'articlefeedbackv5-error-loading-feedback',
@@ -394,18 +396,20 @@ class ArticleFeedbackv5Hooks {
 	 */
 	public static function pushTrackingFieldsToEdit( $editPage, $output ) {
 		$request = $output->getRequest();
-		$tracking = $request->getVal( 'articleFeedbackv5_click_tracking' );
-		$bucketId = $request->getVal( 'articleFeedbackv5_bucket_id' );
-		$ctaId    = $request->getVal( 'articleFeedbackv5_cta_id' );
-		$flinkId  = $request->getVal( 'articleFeedbackv5_f_link_id' );
-		$location = $request->getVal( 'articleFeedbackv5_location' );
-		$token    = $request->getVal( 'articleFeedbackv5_ct_token' );
-		$ctEvent  = $request->getVal( 'articleFeedbackv5_ct_event' );
+		$tracking   = $request->getVal( 'articleFeedbackv5_click_tracking' );
+		$bucketId   = $request->getVal( 'articleFeedbackv5_bucket_id' );
+		$ctaId      = $request->getVal( 'articleFeedbackv5_cta_id' );
+		$flinkId    = $request->getVal( 'articleFeedbackv5_f_link_id' );
+		$experiment = $request->getVal( 'articleFeedbackv5_experiment' );
+		$location   = $request->getVal( 'articleFeedbackv5_location' );
+		$token      = $request->getVal( 'articleFeedbackv5_ct_token' );
+		$ctEvent    = $request->getVal( 'articleFeedbackv5_ct_event' );
 
 		$editPage->editFormTextAfterContent .= Html::hidden( 'articleFeedbackv5_click_tracking', $tracking );
 		$editPage->editFormTextAfterContent .= Html::hidden( 'articleFeedbackv5_bucket_id', $bucketId );
 		$editPage->editFormTextAfterContent .= Html::hidden( 'articleFeedbackv5_cta_id', $ctaId );
 		$editPage->editFormTextAfterContent .= Html::hidden( 'articleFeedbackv5_f_link_id', $flinkId );
+		$editPage->editFormTextAfterContent .= Html::hidden( 'articleFeedbackv5_experiment', $experiment );
 		$editPage->editFormTextAfterContent .= Html::hidden( 'articleFeedbackv5_location', $location );
 		$editPage->editFormTextAfterContent .= Html::hidden( 'articleFeedbackv5_ct_token', $token );
 		$editPage->editFormTextAfterContent .= Html::hidden( 'articleFeedbackv5_ct_event', $ctEvent );
@@ -468,20 +472,25 @@ class ArticleFeedbackv5Hooks {
 			return;
 		}
 
-		$version  = $wgArticleFeedbackv5Tracking['version'];
-		$bucketId = $request->getVal( 'articleFeedbackv5_bucket_id' );
-		$ctaId    = $request->getVal( 'articleFeedbackv5_cta_id' );
-		$flinkId  = $request->getVal( 'articleFeedbackv5_f_link_id' );
-		$location = $request->getVal( 'articleFeedbackv5_location' );
-		$token    = $request->getVal( 'articleFeedbackv5_ct_token' );
-		$ctEvent  = $request->getVal( 'articleFeedbackv5_ct_event' );
+		$version    = $wgArticleFeedbackv5Tracking['version'];
+		$bucketId   = $request->getVal( 'articleFeedbackv5_bucket_id' );
+		$ctaId      = $request->getVal( 'articleFeedbackv5_cta_id' );
+		$flinkId    = $request->getVal( 'articleFeedbackv5_f_link_id' );
+		$experiment = $request->getVal( 'articleFeedbackv5_experiment' );
+		$location   = $request->getVal( 'articleFeedbackv5_location' );
+		$token      = $request->getVal( 'articleFeedbackv5_ct_token' );
+		$ctEvent    = $request->getVal( 'articleFeedbackv5_ct_event' );
 
 		if ( $ctEvent ) {
 			$trackingId = $ctEvent . '-' . $event;
 		} else {
-			$trackingId = 'ext.articleFeedbackv5@' . $version
-				. '-option' . $bucketId . $flinkId
-				. '-cta_' . ( isset( $ctas[$ctaId] ) ? $ctas[$ctaId] : 'unknown' )
+			$trackingId = 'ext.articleFeedbackv5@' . $version;
+			if ( $experiment ) {
+				$trackingId .= '-' . $experiment; // Stage 3 or greater
+			} else {
+				$trackingId .= '-option' . $bucketId . $flinkId; // Prior to stage 3; handles cached js
+			}
+			$trackingId .= '-cta_' . ( isset( $ctas[$ctaId] ) ? $ctas[$ctaId] : 'unknown' )
 				. '-' . $event
 				. '-' . $location;
 		}
