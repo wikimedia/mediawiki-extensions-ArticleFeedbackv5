9b42ff3eb06787f9dc7df50c9695736da2a9dcf2
Matthias Mullie <git@mullie.eu>
2012-05-02 16:06:11 +0200

    bug 36283 - hide 'view activity' when there is none; different help-links per usergroup
    Change-Id: I7379dd517c5c6f92f71e0217923dc631ed91b22a

--- ArticleFeedbackv5.fetch.php
+++ ArticleFeedbackv5.fetch.php
@@ -141,6 +141,8 @@ class ArticleFeedbackv5Fetch {
 	 *                  }
 	 */
 	public function run() {
+		global $wgUser; // we need to check permissions in here for suppressionlog stuff
+
 		$dbr = wfGetDB( DB_SLAVE );
 
 		$result = new stdClass;
@@ -272,13 +274,42 @@ class ArticleFeedbackv5Fetch {
 			array_pop( $ids );
 		}
 
+		/* We'll try to determine if the there are log entries (= activity) for this feedback, but the visible activity
+		 depend on the user's permissions */
+
+		// get the string title for the page
+		$page = Title::newFromID( $this->pageId );
+		if ( !$page ) {
+			$this->dieUsage( "Page for feedback does not exist", "invalidfeedbackid" );
+		}
+		$title = $page->getPrefixedDBKey();
+
+		// get afv5 log items PLUS suppress log
+		if ( $wgUser->isAllowed( 'aftv5-delete-feedback' ) ) {
+			$whereLogging = array(
+					"logging.log_type = 'articlefeedbackv5'
+					OR (logging.log_type = 'suppress'
+					AND logging.log_action IN ('oversight', 'unoversight', 'decline', 'request', 'unrequest'))",
+					"logging.log_namespace" => NS_SPECIAL,
+					"logging.log_title = CONCAT('ArticleFeedbackv5/$title/', af_id)"
+			);
+
+			// get only afv5 log items
+		} else {
+			$whereLogging = array (
+					"logging.log_type" => "articlefeedbackv5",
+					"logging.log_namespace" => NS_SPECIAL,
+					"logging.log_title = CONCAT('ArticleFeedbackv5/$title/', af_id)"
+			);
+		}
+
 		// Select rows
 		$rows  = $dbr->select(
 			array( 'aft_article_feedback',
 				'rating' => 'aft_article_answer',
 				'answer' => 'aft_article_answer',
 				'aft_article_field',
-				'aft_article_field_option', 'user', 'page'
+				'aft_article_field_option', 'user', 'page', 'logging'
 			),
 			array( 'af_id', 'af_form_id', 'afi_name', 'afo_name',
 				'answer.aa_response_text', 'answer.aa_response_boolean',
@@ -293,7 +324,8 @@ class ArticleFeedbackv5Fetch {
 				'af_is_featured', 'af_is_resolved',
 				'af_last_status',
 				'af_last_status_user_id',
-				'af_last_status_timestamp'
+				'af_last_status_timestamp',
+				'logging.log_timestamp'
 			),
 			array( 'af_id' => $ids ),
 			__METHOD__,
@@ -318,6 +350,10 @@ class ArticleFeedbackv5Fetch {
 				),
 				'page' => array(
 					'JOIN', 'page_id = af_page_id'
+				),
+				'logging' => array(
+					'LEFT JOIN',
+					$whereLogging
 				)
 			)
 		);
