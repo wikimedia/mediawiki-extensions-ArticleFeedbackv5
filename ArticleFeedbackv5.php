0d4092cb2cdd94e2af899065b1091cb6b04b6b94
rsterbin <reha@omniti.com>
2012-05-18 14:13:54 -0400

    Update bucketing for forms to 100% option 1
    Change-Id: I1c4eb0db0f16b31b51e527e873db738dc827797a

--- ArticleFeedbackv5.php
+++ ArticleFeedbackv5.php
@@ -143,17 +143,17 @@ $wgArticleFeedbackv5DisplayBuckets = array(
 	// the new odds are applied to everyone, not just people who have yet to be
 	// placed in a bucket.
 	'buckets' => array(
-		'zero'  => 33,
-		'one'   => 33,
+		'zero'  => 0,
+		'one'   => 100,
 		'two'   => 0,
 		'three' => 0,
-		'four'  => 33,
+		'four'  => 0,
 		'five'  => 0,
 	),
 	// This version number is added to all tracking event names, so that
 	// changes in the software don't corrupt the data being collected. Bump
 	// this when you want to start a new "experiment".
-	'version' => 2,
+	'version' => 3,
 	// Let users be tracked for a month, and then rebucket them, allowing some
 	// churn.
 	'expires' => 30,
