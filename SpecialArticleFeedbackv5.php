0981daf3acdcd48233511efc742331b70708cd0e
rsterbin <reha@omniti.com>
2012-05-22 23:00:01 -0400

    Metrics stage 4 / feedback page click tracking (bug 36650)
    - ArticleFeedbackv5.php: Set the clicktracking version to 4
    - SpecialArticleFeedbackv5.php
        - Accepts a parameter "ref", indicating where the request came from
        - Passes afReferral to the javascript
        - Passes afIsAnon to the javascript, for user privileges calculation
    - modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.special.js:
        - New properties clickTracking, userType, and referral
        - New methods checkClickTracking(), prefix(), trackClick(),
          trackActionURL(), trackingUrl(), and getFilterName()
        - Updated setup() to check the status of click tracking, determine the user
          type, set the referral, and track an impression
        - Updated setBinds() to track filter changes
        - Bug fix: added a preventDefault() to the filter click event, to keep
          those hashes from showing up in the url
        - Renamed "aftUrl" property to "specialUrl" for clarity
        - Put it in the properties section, along with a new property "permalink"
        - Custom title for CTA5, including permalink for the new feedback
        - Added tracking for the button and link, and an extra impression track
          using the stage4 format
    - Updated ApiArticleFeedbackv5 to send both the special page url and the
      relevant permalink, and added ?ref=cta to both
    - Updated talk page link to send ?ref=talk
    - Added clicktracking to the talk page link (impression + click)
      * NB: Uses the talk page's title, namespace, and revision, not the article's.
    - Added the survey button to the feedback page
    - Bug fix in ArticleFeedbackv5Render::renderBucket* functions: generated a
      notice if there was no comment
    
    Patch set 2:
    
    - Added message doc lines for articlefeedbackv5-cta5-confirmation-followup and
      articlefeedbackv5-cta5-confirmation-followup-linktext
    - Dropped extraneous variable afIsAnon
    - Updated tracking additional data to use wgPageName instead of wgTitle, so we
      get the namespace as well (front end and talk; special page still uses the
      page ID instead of its name)
    
    Change-Id: Ic9077b66138eabcd4510658453c973f05fa70b37

--- SpecialArticleFeedbackv5.php
+++ SpecialArticleFeedbackv5.php
@@ -163,7 +163,10 @@ class SpecialArticleFeedbackv5 extends UnlistedSpecialPage {
 	 * @param $param string the parameter passed in the url
 	 */
 	public function execute( $param ) {
-		global $wgArticleFeedbackv5DashboardCategory, $wgArticleFeedbackv5DefaultSorts, $wgArticleFeedbackv5DefaultFilters, $wgUser;
+		global $wgArticleFeedbackv5DashboardCategory,
+			$wgArticleFeedbackv5DefaultSorts, $wgArticleFeedbackv5DefaultFilters,
+			$wgUser, $wgRequest;
+
 		$out = $this->getOutput();
 		$out->addModuleStyles( 'ext.articleFeedbackv5.dashboard' );
 		$out->addModuleStyles( 'jquery.articleFeedbackv5.special' );
@@ -274,6 +277,7 @@ class SpecialArticleFeedbackv5 extends UnlistedSpecialPage {
 
 		// JS variables
 		$out->addJsConfigVars( 'afPageId', $this->pageId );
+		$out->addJsConfigVars( 'afReferral', $wgRequest->getText( 'ref', 'url' ) );
 		// Only show the abuse counts to editors (ie, anyone allowed to
 		// hide content).
 		if ( $wgUser->isAllowed( 'aftv5-see-hidden-feedback' ) ) {
@@ -495,6 +499,21 @@ class SpecialArticleFeedbackv5 extends UnlistedSpecialPage {
 					. Html::closeElement( 'div' )
 				);
 			}
+		}
+
+		// Survey button
+		global $wgArticleFeedbackv5SpecialPageSurveyUrl;
+		if ( $wgArticleFeedbackv5SpecialPageSurveyUrl ) {
+			$out->addHTML(
+				// <a href="{survey-url}" target="_blank" class="articleFeedbackv5-survey-button">
+				//   {msg:articlefeedbackv5-special-survey-button-text}
+				// </span>
+				Html::element( 'a', array(
+					'href'   => $wgArticleFeedbackv5SpecialPageSurveyUrl,
+					'target' => '_blank',
+					'class'  => 'articleFeedbackv5-survey-button',
+				), $this->msg( 'articlefeedbackv5-special-survey-button-text' )->text() )
+			);
 		}
 
 		// BETA notice
