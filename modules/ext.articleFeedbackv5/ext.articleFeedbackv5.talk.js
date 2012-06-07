5d35089b7d95275dc3f18604d32354e7da6ee271
Reha Sterbin <reha@omniti.com>
2012-06-06 20:03:14 -0400

    Refactored click tracking
    - Moved all the scattered tracking methods into $.aftTrack
    - Made it its own module, jquery.articleFeedbackv5.track
    - Updated ArticleFeedbackv5Hooks to add it, and to replace every instance of
      the jquery.clickTracking dependency with it
    - Updated ext.articleFeedbackv5.talk.js, ext.articleFeedbackv5.js,
      jquery.articleFeedbackv5.js, and jquery.articleFeedbackv5.special.js to call
      $.aftTrack for prefix(), trackClick(), and trackingUrl() calls rather than
      using internal ones
    - Except for trackClick() and trackingUrl() in jquery.articleFeedbackv5.js,
      where the methods were kept to auto-add the experiment as a prefix
    - Also in jquery.articleFeedbackv5.js, moved trackClick() next to trackingUrl()
      for clarity
    - Bug fixes for CTA5:
        - Was using a hardcoded CTA name rather than $.articleFeedbackv5.ctaName(),
          and a different one from the one in ctaName()
        - Was not reporting overlay vs bottom
        - Was double-tracking impressions (one as just impression, and one as
          impression-overlay or impression-bottom)
    
    Patch set 2:
    - Form impressions are now tracked at 1%
    - Clicktracking can now be turned off for the front end and special page
      independently
    
    Change-Id: I42665a5890f36ae1e3a0fc40c48e15262c4db2cd

--- modules/ext.articleFeedbackv5/ext.articleFeedbackv5.talk.js
+++ modules/ext.articleFeedbackv5/ext.articleFeedbackv5.talk.js
@@ -24,50 +24,10 @@ jQuery( function( $ ) {
 	// Check if the talk page link can be shown
 	if ( mw.config.get( 'wgArticleFeedbackv5TalkPageLink' ) ) {
 
-		// Click tracking methods
-		var clickTrackingOn = function () {
-			var b = mw.config.get( 'wgArticleFeedbackv5Tracking' );
-			if ( b.buckets.ignore == 100 && b.buckets.track == 0 ) {
-				return false;
-			}
-			if ( b.buckets.ignore == 0 && b.buckets.track == 100 ) {
-				return true;
-			}
-			var key = 'ext.articleFeedbackv5@' + b.version + '-tracking'
-			return ( 'track' === mw.user.bucket( key, b ) );
-		};
-
-		var prefix = function ( track_id ) {
-			var version = mw.config.get( 'wgArticleFeedbackv5Tracking' ).version || 0;
-			return 'ext.articleFeedbackv5@' + version + '-' + track_id;
-		};
-
-		var trackingUrl = function ( url, track_id ) {
-			if ( clickTrackingOn() ) {
-				// NB: Using the talk page's namespace, title, and rev id, not
-				// the article's as in the front end tracking
-				return mw.config.get( 'wgScriptPath' ) + '/api.php?' + $.param( {
-					'action': 'clicktracking',
-					'format' : 'json',
-					'eventid': prefix( track_id ),
-					'namespacenumber': mw.config.get( 'wgNamespaceNumber' ),
-					'token': $.cookie( 'clicktracking-session' ),
-					'additional': mw.config.get( 'wgPageName' ) + '|' + mw.config.get( 'wgCurRevisionId' ),
-					'redirectto': url
-				} );
-			} else {
-				return url;
-			}
-		};
-
-		var trackClick = function ( track_id ) {
-			if ( clickTrackingOn() && $.isFunction( $.trackActionWithInfo ) ) {
-				$.trackActionWithInfo(
-					prefix( track_id ),
-					mw.config.get( 'wgPageName' ) + '|' + mw.config.get( 'wgCurRevisionId' )
-				);
-			}
-		};
+		// Initialize clicktracking
+		// NB: Using the talk page's namespace, title, and rev id, not
+		// the article's as in the front end tracking
+		$.aftTrack.init();
 
 		// Build the url to the Special:ArticleFeedbackv5 page
 		var params = { ref: 'talk' };
@@ -81,11 +41,11 @@ jQuery( function( $ ) {
 		link.text( mw.msg( 'articlefeedbackv5-talk-view-feedback' ) );
 		link.html( link.html() + ' &raquo;' );
 		link.attr( 'title', mw.msg( 'articlefeedbackv5-talk-view-feedback' ) );
-		link.attr( 'href', trackingUrl( url, track_id ) );
+		link.attr( 'href', $.aftTrack.trackingUrl( url, track_id ) );
 		$( '#firstHeading' ).append( link );
 
 		// Track an impression
-		trackClick( 'talk_page_view_feedback-impression' );
+		$.aftTrack.trackClick( 'talk_page_view_feedback-impression' );
 	}
 
 } );
---
+++ modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.track.js
@@ -0,0 +1,176 @@
+/**
+ * ArticleFeedback tracking plugin
+ *
+ * This file creates the plugin that will be used to track usage of the Article
+ * Feedback tool.
+ *
+ * @package    ArticleFeedback
+ * @subpackage Resources
+ * @author     Reha Sterbin <reha@omniti.com>
+ * @version    $Id$
+ */
+
+( function ( $ ) {
+
+// {{{ aftTrack definition
+
+	$.aftTrack = {};
+
+	// {{{ Properties
+
+	/**
+	 * Are we tracking clicks?
+	 */
+	$.aftTrack.clickTrackingOn = false;
+
+	/**
+	 * The page name we'll be sending with the tracking
+	 */
+	$.aftTrack.pageName = mw.config.get( 'wgPageName' );
+
+	/**
+	 * The revision ID we'll be sending with the tracking
+	 */
+	$.aftTrack.revisionId = mw.config.get( 'wgCurRevisionId' );
+
+	/**
+	 * Is this the front end or the special page?
+	 */
+	$.aftTrack.isSpecial = false;
+
+	// }}}
+	// {{{ init
+
+	/**
+	 * Initializes the object
+	 *
+	 * The init method sets up the object once the plugin has been called.
+	 *
+	 * @param config object the config object
+	 */
+	$.aftTrack.init = function ( config ) {
+		// Fill in options from the config
+		config = config || {};
+		if ( 'pageName' in config ) {
+			$.aftTrack.pageName = config.pageName;
+		}
+		if ( 'revisionId' in config ) {
+			$.aftTrack.revisionId = config.revisionId;
+		}
+		if ( 'isSpecial' in config && config.isSpecial ) {
+			$.aftTrack.isSpecial = true;
+		}
+		// Are we tracking clicks?
+		var b = mw.config.get( 'wgArticleFeedbackv5Tracking' );
+		var key = 'ext.articleFeedbackv5@' + b.version + '-tracking'
+		var setting = mw.user.bucket( key, b );
+		if ( setting == 'track' ||
+			( setting == 'track-special' && $.aftTrack.isSpecial ) ||
+			( setting == 'track-front' && !$.aftTrack.isSpecial ) ) {
+			$.aftTrack.clickTrackingOn = true;
+		} else {
+			$.aftTrack.clickTrackingOn = false;
+		}
+	};
+
+	// }}}
+	// {{{ prefix
+
+	/**
+	 * Utility method: Prefixes a key for tracking event names with extension and
+	 * version information
+	 *
+	 * @param  key    string name of event to prefix
+	 * @return string prefixed event name
+	 */
+	$.aftTrack.prefix = function ( key ) {
+		var version = mw.config.get( 'wgArticleFeedbackv5Tracking' ).version || 0;
+		return 'ext.articleFeedbackv5@' + version + '-' + key;
+	};
+
+	// }}}
+	// {{{ additional
+
+	/**
+	 * Builds the additional data to pass with events
+	 *
+	 * @return string the additional data string
+	 */
+	$.aftTrack.additional = function () {
+		var tmp = new Array();
+		if ( $.aftTrack.pageName != '' ) {
+			tmp.push( $.aftTrack.pageName );
+		}
+		if ( $.aftTrack.revisionId != 0 ) {
+			tmp.push( $.aftTrack.revisionId );
+		}
+		return tmp.join( '|' );
+	};
+
+	// }}}
+	// {{{ trackClick
+
+	/**
+	 * Tracks a click
+	 *
+	 * @param trackingId string the tracking ID
+	 */
+	$.aftTrack.trackClick = function ( trackingId ) {
+		if ( $.aftTrack.clickTrackingOn && $.isFunction( $.trackActionWithInfo ) ) {
+			$.trackActionWithInfo(
+				$.aftTrack.prefix( trackingId ),
+				$.aftTrack.additional()
+			);
+		}
+	};
+
+	// }}}
+	// {{{ trackingUrl
+
+	/**
+	 * Creates a URL that tracks a particular click
+	 *
+	 * @param url        string the url so far
+	 * @param trackingId string the tracking ID
+	 */
+	$.aftTrack.trackingUrl = function ( url, trackingId ) {
+		if ( $.aftTrack.clickTrackingOn ) {
+			return $.aftTrack.trackActionURL( url, $.aftTrack.prefix( trackingId ) );
+		} else {
+			return url;
+		}
+	};
+
+	// }}}
+	// {{{ trackActionURL
+
+	/**
+	 * Rewrites a URL to one that runs through the ClickTracking API module
+	 * which registers the event and redirects to the real URL
+	 *
+	 * This is a copy of the one out of the clicktracking javascript API
+	 * we have to do our own because there is no "additional" option in that
+	 * API which we need for the article title
+	 *
+	 * @param string url url to redirect to
+	 * @param string id  the tracking id
+	 */
+	$.aftTrack.trackActionURL = function( url, id ) {
+		return mw.config.get( 'wgScriptPath' ) + '/api.php?' + $.param( {
+			'action': 'clicktracking',
+			'format' : 'json',
+			'eventid': id,
+			'namespacenumber': mw.config.get( 'wgNamespaceNumber' ),
+			'token': $.cookie( 'clicktracking-session' ),
+			'additional': $.aftTrack.additional(),
+			'redirectto': url
+		} );
+	};
+
+	// }}}
+
+// }}}
+
+} )( jQuery );
+
+
