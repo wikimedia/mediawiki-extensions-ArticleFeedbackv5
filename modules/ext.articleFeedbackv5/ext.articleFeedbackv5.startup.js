c3f6df7fe35b4bef294e4353e9c0a4dbb2217c8f
rsterbin <reha@omniti.com>
2012-06-08 09:43:06 -0400

    Reinstated the javascript-based check for AFTv5, for the time being
    Change-Id: I9cb7ba0aa5d0972b86755b56fbc807ca850d64d0

--- modules/ext.articleFeedbackv5/ext.articleFeedbackv5.startup.js
+++ modules/ext.articleFeedbackv5/ext.articleFeedbackv5.startup.js
@@ -32,10 +32,63 @@ jQuery( function( $ ) {
 		return;
 	}
 
-	mw.loader.load( 'ext.articleFeedbackv5' );
-	// Load the IE-specific module
-	if( navigator.appVersion.indexOf( 'MSIE 7' ) != -1 ) {
-		mw.loader.load( 'ext.articleFeedbackv5.ie' );
-	}
+   // Load check, is this page ArticleFeedbackv5-enabled ?
+   // Keep in sync with ApiArticleFeedbackv5.php
+   if (
+	   // Only on pages in namespaces where it is enabled
+	   $.inArray( mw.config.get( 'wgNamespaceNumber' ), mw.config.get( 'wgArticleFeedbackv5Namespaces', [] ) ) > -1
+	   // Existing pages
+	   && mw.config.get( 'wgArticleId' ) > 0
+	   // View pages
+	   && ( mw.config.get( 'wgAction' ) == 'view' || mw.config.get( 'wgAction' ) == 'purge' )
+	   // If user is logged in, showing on action=purge is OK,
+	   // but if user is logged out, action=purge shows a form instead of the article,
+	   // so return false in that case.
+	   && !( mw.config.get( 'wgAction' ) == 'purge' && mw.user.anonymous() )
+	   // Current revision
+	   && mw.util.getParamValue( 'diff' ) == null
+	   && mw.util.getParamValue( 'oldid' ) == null
+	   // Not disabled via preferences
+	   && !mw.user.options.get( 'articlefeedback-disable' )
+	   // Not viewing a redirect
+	   && mw.util.getParamValue( 'redirect' ) != 'no'
+	   // Not viewing the printable version
+	   && mw.util.getParamValue( 'printable' ) != 'yes'
+   ) {
+	   // Collect categories for intersection tests
+	   // Clone the arrays so we can safely modify them
+	   var categories = {
+		   'include': [].concat( mw.config.get( 'wgArticleFeedbackv5Categories', [] ) ),
+		   'exclude': [].concat( mw.config.get( 'wgArticleFeedbackv5BlacklistCategories', [] ) ),
+		   'current': [].concat( mw.config.get( 'wgCategories', [] ) )
+	   };
+
+	   var enable = false;
+	   for( var i = 0; i < categories['current'].length; i++ ) {
+		   // Categories are configured with underscores, but article's categories are returned with
+		   // spaces instead. Revert to underscores here for sane comparison.
+		   categories['current'][i] = categories['current'][i].replace(/\s/gi, '_');
+		   // Check exclusion - exclusion overrides everything else
+		   if( $.inArray( categories['current'][i], categories.exclude ) > -1 ) {
+			   // Blacklist overrides everything else
+			   return;
+		   }
+		   if( $.inArray( categories['current'][i], categories.include ) > -1 ) {
+			   // One match is enough for include, however we are iterating on the 'current'
+			   // categories, and others might be blacklisted - so continue iterating
+			   enable = true;
+		   }
+	   }
+
+	   // Lazy loading
+	   if ( enable ) {
+		   mw.loader.load( 'ext.articleFeedbackv5' );
+		   // Load the IE-specific module
+		   if( navigator.appVersion.indexOf( 'MSIE 7' ) != -1 ) {
+			   mw.loader.load( 'ext.articleFeedbackv5.ie' );
+		   }
+	   }
+
+   }
 
 } );
