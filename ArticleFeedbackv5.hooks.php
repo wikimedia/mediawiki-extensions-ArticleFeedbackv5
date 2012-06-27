e4919da32b860d6b61ff902c7453972ba0581241
mlitn <git@mullie.eu>
2012-06-27 14:34:52 +0200

    Update MyContribs hook to reflect the changes in code:
    - added parameter
    - moved article are wrapper in <li> in core again
    
    Change-Id: I547c57836e08c77f75b676c8c85bd846ec52999e

--- ArticleFeedbackv5.hooks.php
+++ ArticleFeedbackv5.hooks.php
@@ -774,9 +774,10 @@ class ArticleFeedbackv5Hooks {
 	 * @param $page SpecialPage object for contributions
 	 * @param $ret string the HTML line
 	 * @param $row Row the DB row for this line
+	 * @param $classes the classes to add to the surrounding <li>
 	 * @return bool
 	 */
-	public static function contributionsLineEnding( &$page, &$ret, $row ) {
+	public static function contributionsLineEnding( $page, &$ret, $row, &$classes ) {
 		if ( !isset( $row->af_id ) || $row->af_id === '' ) {
 			return true;
 		}
@@ -843,8 +844,7 @@ class ArticleFeedbackv5Hooks {
 			$status .= ' - ' . implode( ', ', $actions);
 		}
 
-		$ret = "{$d} {$chardiff} {$feedback} {$status}";
-		$ret = "<li>$ret</li>\n";
+		$ret = "{$d} {$chardiff} {$feedback} {$status}\n";
 
 		return true;
 	}
