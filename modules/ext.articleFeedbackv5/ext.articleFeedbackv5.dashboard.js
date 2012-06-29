9cc4f007e0290b875604650131a359d51bb9985e
rsterbin <reha@omniti.com>
2012-06-29 18:32:33 -0400

    Refactored the javascript side of AFTv5 verification
    - Added new module jquery.articleFeedbackv5.verify
    - Updated ext.articleFeedbackv5.startup.js, ext.articleFeedbackv5.dashboard.js,
      and ext.articleFeedbackv5.talk.js to use it rather than duplicating code
    - Updated ArticleFeedbackv5.hooks.php to make startup, dashboard, and talk
      modules depend on the new verify module
    
    Patch set 2:
    - Exception for the cached special page was removed in master but not on the
      omniti branch; follow the new rules
    
    Change-Id: I76b099080c703716135a02118d2f882a3a73d5d2

--- modules/ext.articleFeedbackv5/ext.articleFeedbackv5.dashboard.js
+++ modules/ext.articleFeedbackv5/ext.articleFeedbackv5.dashboard.js
@@ -1,5 +1,5 @@
 /*
- * This script determines if Special:ArticleFeedbackv5/<Article> should display 
+ * This script determines if Special:ArticleFeedbackv5/<Article> should display
  * the feedback dashboard or not.
  */
 
@@ -17,59 +17,22 @@ aft5_debug = function( any ) {
 /*** Main entry point ***/
 jQuery( function( $ ) {
 
-	var ua = navigator.userAgent.toLowerCase();
-	// Rule out MSIE 6/7, iPhone, iPod, iPad, Android
-	if(
-		(ua.indexOf( 'msie 6' ) != -1) ||
-		(ua.indexOf( 'msie 7' ) != -1) ||
-		(ua.indexOf( 'firefox/2') != -1) ||
-		(ua.indexOf( 'firefox 2') != -1) ||
-		(ua.indexOf( 'android' ) != -1) ||
-		(ua.indexOf( 'iphone' ) != -1) ||
-		(ua.indexOf( 'ipod' ) != -1 ) ||
-		(ua.indexOf( 'ipad' ) != -1)
-	) {
-		// Remove the extension's output & replace it with a warning that the browser isn't supported
-		var warning = $( '#articlefeedbackv5-header-message' ).text( mw.msg( 'articlefeedbackv5-unsupported-message' ) );
-		$( '#articleFeedbackv5-special-wrap' ).empty().append( warning );
-		return;
-	}
-
-	var enable = false;
-	var whitelist = mw.config.get( 'aftv5Whitelist', -1 );
-	var v4odds = mw.config.get( 'wgArticleFeedbackLotteryOdds', -1 );
-	var pageId = mw.config.get( 'aftv5PageId', -1 );
-
-	// If we're on the Central Feedback page (Special:ArticleFeedbackv5 with
-	// no specific article specified), show the feedback dashboard.
-	if ( mw.config.get( 'wgNamespaceNumber' ) == -1 && pageId == 0 ) {
-		enable = true;
-	}
-
-	// If the article is whitelisted, show the feedback dashboard.
-	else if ( whitelist ) { 
-		enable = true;
-	}
-
-	// If the article is an AFTv4 lottery loser, show the feedback dashboard.
-	// In other words, the AFTv5 lottery is the inverse of the AFTv4 lottery.
-	else if ( !(( Number( pageId ) % 1000 ) < Number( v4odds ) * 10) ) {
-		enable = true
-	}
-
-	// no vars = cached
-	// -> either it's really old, from when allowance code was only in JS = probably don't show
-	// -> either it's not too old, so PHP allowance let it through = show
-	// there's no way to know though, so we just won't show a thing, for now
-
-	// blacklist has been handled by PHP; if it's blacklisted, this JS won't be loaded
-
+	// Is AFT enabled here?
+	var enable = $.aftVerify.verify( 'special' );
 	if ( !enable ) {
-		// Remove the extension's output & replace it with a warning that
-		// feedback is disabled for the page.
-		var warning = $( '#articlefeedbackv5-header-message' ).text( mw.msg( 'articlefeedbackv5-page-disabled' ) );
-		$( '#articleFeedbackv5-special-wrap' ).empty().append( warning );
-		return;
+		if ( $.aftVerify.checks.useragent === false ) {
+			// Remove the extension's output & replace it with a warning that the browser
+			// isn't supported
+			var warning = $( '#articlefeedbackv5-header-message' ).text( mw.msg( 'articlefeedbackv5-unsupported-message' ) );
+			$( '#articleFeedbackv5-special-wrap' ).empty().append( warning );
+			return;
+		} else {
+			// Remove the extension's output & replace it with a warning that
+			// feedback is disabled for the page.
+			var warning = $( '#articlefeedbackv5-header-message' ).text( mw.msg( 'articlefeedbackv5-page-disabled' ) );
+			$( '#articleFeedbackv5-special-wrap' ).empty().append( warning );
+			return;
+		}
 	}
 
 	// Otherwise, we're good to go!
---
+++ modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.verify.js
@@ -0,0 +1,248 @@
+/**
+ * ArticleFeedback verification plugin
+ *
+ * This file checks to make sure that AFT is allowed on the current page.
+ *
+ * @package    ArticleFeedback
+ * @subpackage Resources
+ * @author     Reha Sterbin <reha@omniti.com>
+ * @version    $Id$
+ */
+
+( function ( $ ) {
+
+// {{{ aftVerify definition
+
+	$.aftVerify = {};
+
+	// {{{ Properties
+
+	/**
+	 * The current namespace
+	 */
+	$.aftVerify.namespace = -2;
+
+	/**
+	 * The article's whitelist status
+	 */
+	$.aftVerify.whitelist = -1;
+
+	/**
+	 * The page ID
+	 */
+	$.aftVerify.pageId = -1;
+
+	/**
+	 * The current location
+	 *
+	 * Can be: article, talk, special, or unknown
+	 */
+	$.aftVerify.location = 'unknown';
+
+	/**
+	 * Whether AFT should be enabled on this page
+	 */
+	$.aftVerify.enabled = undefined;
+
+	/**
+	 * The results of individual checks
+	 */
+	$.aftVerify.checks = {
+		whitelist: undefined,
+		lottery:   undefined,
+		useragent: undefined,
+		article:   undefined
+	};
+
+	// }}}
+	// {{{ verify
+
+	/**
+	 * Runs verification
+	 *
+	 * @param  location string  the place from which this is being called
+	 * @return bool     whether AFTv5 is enabled for this page
+	 */
+	$.aftVerify.verify = function ( location ) {
+		// Initialize
+		$.aftVerify.enabled = undefined;
+		$.aftVerify.checks.whitelist = undefined;
+		$.aftVerify.checks.lottery = undefined;
+		$.aftVerify.checks.useragent = undefined;
+		$.aftVerify.checks.article = undefined;
+
+		// Pull info passed in
+		$.aftVerify.location = location;
+		$.aftVerify.namespace = mw.config.get( 'wgNamespaceNumber', -2 );
+		$.aftVerify.whitelist = mw.config.get( 'aftv5Whitelist', -1 );
+		if ( $.aftVerify.location == 'article' ) {
+			// Articles use the built-in page ID
+			$.aftVerify.pageId = mw.config.get( 'wgArticleId', -1 );
+		} else {
+			// Talk and special pages have one passed in
+			$.aftVerify.pageId = mw.config.get( 'aftv5PageId', -1 );
+		}
+
+		// Case 1: the html is cached
+		if ( $.aftVerify.whitelist == -1 ) {
+			if ( $.aftVerify.location == 'article' ) {
+				// We can double-check, so do that
+				$.aftVerify.enabled = $.aftVerify.checkFull();
+			} else {
+				// Everywhere else: don't show it
+				$.aftVerify.enabled = false;
+			}
+
+		// Case 2: the article is whitelisted
+		} else if ( $.aftVerify.whitelist ) {
+			$.aftVerify.checks.whitelist = true;
+			$.aftVerify.enabled = true;
+
+		// Case 3: the article is not whitelisted
+		} else {
+			$.aftVerify.checks.whitelist = false;
+			$.aftVerify.enabled = $.aftVerify.checkLottery();
+		}
+
+		// Check the user agent
+		if ( $.aftVerify.enabled ) {
+			$.aftVerify.enabled = $.aftVerify.checkUserAgent();
+		}
+
+		return $.aftVerify.enabled;
+	};
+
+	// }}}
+	// {{{ checkFull
+
+	/**
+	 * If this is a main article, we can check everything at once
+	 *
+	 * @return bool whether AFTv5 is enabled
+	 */
+	$.aftVerify.checkFull = function () {
+		if (
+			// Only on pages in namespaces where it is enabled
+			$.inArray( mw.config.get( 'wgNamespaceNumber' ), mw.config.get( 'wgArticleFeedbackv5Namespaces', [] ) ) > -1
+			// Existing pages
+			&& mw.config.get( 'wgArticleId' ) > 0
+			// View pages
+			&& ( mw.config.get( 'wgAction' ) == 'view' || mw.config.get( 'wgAction' ) == 'purge' )
+			// If user is logged in, showing on action=purge is OK,
+			// but if user is logged out, action=purge shows a form instead of the article,
+			// so return false in that case.
+			&& !( mw.config.get( 'wgAction' ) == 'purge' && mw.user.anonymous() )
+			// Current revision
+			&& mw.util.getParamValue( 'diff' ) == null
+			&& mw.util.getParamValue( 'oldid' ) == null
+			// Not disabled via preferences
+			&& !mw.user.options.get( 'articlefeedback-disable' )
+			// Not viewing a redirect
+			&& mw.util.getParamValue( 'redirect' ) != 'no'
+			// Not viewing the printable version
+			&& mw.util.getParamValue( 'printable' ) != 'yes'
+		) {
+			$.aftVerify.checks.article = true;
+
+			// Collect categories for intersection tests
+			// Clone the arrays so we can safely modify them
+			var enable = true;
+			var categories = {
+				'include': [].concat( mw.config.get( 'wgArticleFeedbackv5Categories', [] ) ),
+				'exclude': [].concat( mw.config.get( 'wgArticleFeedbackv5BlacklistCategories', [] ) ),
+				'current': [].concat( mw.config.get( 'wgCategories', [] ) )
+			};
+			for ( var i = 0; i < categories['current'].length; i++ ) {
+				// Categories are configured with underscores, but article's categories are returned with
+				// spaces instead. Revert to underscores here for sane comparison.
+				categories['current'][i] = categories['current'][i].replace(/\s/gi, '_');
+				// Check exclusion - exclusion overrides everything else
+				if ( $.inArray( categories['current'][i], categories.exclude ) > -1 ) {
+					// Blacklist overrides everything else
+					return;
+				}
+				if ( $.inArray( categories['current'][i], categories.include ) > -1 ) {
+					// One match is enough for include, however we are iterating on the 'current'
+					// categories, and others might be blacklisted - so continue iterating
+					enable = true;
+				}
+			}
+			if ( enable ) {
+				$.aftVerify.checks.whitelist = true;
+				return true;
+			} else {
+				return $.aftVerify.checkLottery();
+			}
+
+		} else {
+			$.aftVerify.checks.article = false;
+			return false;
+		}
+	};
+
+	// }}}
+	// {{{ checkLottery
+
+	/**
+	 * Check the lottery
+	 *
+	 * AFT5's lottery is the inverse of AFT4's.
+	 *
+	 * @return bool whether AFTv5 is enabled
+	 */
+	$.aftVerify.checkLottery = function () {
+		// No page id, no check
+		if ( $.aftVerify.pageId < 1 ) {
+			// Special pages: zero = central feedback page
+			if ( $.aftVerify.location == 'special' && $.aftVerify.pageId == 0 ) {
+				$.aftVerify.checks.lottery = true;
+				return true;
+			} else {
+				$.aftVerify.checks.lottery = false;
+				return false;
+			}
+		}
+		// Lottery
+		var v4odds = mw.config.get( 'wgArticleFeedbackLotteryOdds', 0 );
+		if ( !( ( Number( $.aftVerify.pageId ) % 1000 ) < Number( v4odds ) * 10 ) ) {
+			$.aftVerify.checks.lottery = true;
+			return true;
+		} else {
+			$.aftVerify.checks.lottery = false;
+			return false;
+		}
+	};
+
+	// }}}
+	// {{{ checkUserAgent
+
+	/**
+	 * Check the user agent
+	 */
+	$.aftVerify.checkUserAgent = function () {
+		var ua = navigator.userAgent.toLowerCase();
+		// Rule out MSIE 6/7, FF2, iPhone, iPod, iPad, Android
+		if (
+			(ua.indexOf( 'msie 6' ) != -1) ||
+			(ua.indexOf( 'msie 7' ) != -1) ||
+			(ua.indexOf( 'firefox/2') != -1) ||
+			(ua.indexOf( 'firefox 2') != -1) ||
+			(ua.indexOf( 'android' ) != -1) ||
+			(ua.indexOf( 'iphone' ) != -1) ||
+			(ua.indexOf( 'ipod' ) != -1 ) ||
+			(ua.indexOf( 'ipad' ) != -1)
+		) {
+			$.aftVerify.checks.useragent = false;
+			return false;
+		} else {
+			$.aftVerify.checks.useragent = true;
+			return true;
+		}
+	};
+
+	// }}}
+
+// }}}
+
+} )( jQuery );
+
