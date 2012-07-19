14f54de96f838e62c352504850a13428b3493584
mlitn <git@mullie.eu>
2012-07-19 17:17:25 +0200

    second link on contribs should go to special page with post on #top
    Change-Id: I986cb0658d6ebc66736c48d48954edf3da15f903

--- ArticleFeedbackv5.hooks.php
+++ ArticleFeedbackv5.hooks.php
@@ -801,6 +801,7 @@ class ArticleFeedbackv5Hooks {
 		$lang = $page->getLanguage();
 		$user = $page->getUser();
 		$feedbackTitle = SpecialPage::getTitleFor( 'ArticleFeedbackv5', $pageTitle->getPrefixedDBkey() . "/$row->af_id" );
+		$feedbackCentralPageTitle = SpecialPage::getTitleFor( 'ArticleFeedbackv5', $pageTitle->getPrefixedDBkey() . "#$row->af_id" );
 
 		// date
 		$date = $lang->userTimeAndDate( $row->af_created, $user );
@@ -816,7 +817,7 @@ class ArticleFeedbackv5Hooks {
 		$chardiff = ' . . ' . ChangesList::showCharacterDifference( 0, strlen( $row->af_comment ) ) . ' . . ';
 
 		// article feedback is given on
-		$article = $lang->getDirMark() . wfMessage( 'articlefeedbackv5-contribs-feedback', $feedbackTitle->getPrefixedDBkey(), $pageTitle->getPrefixedText() )->parse();
+		$article = $lang->getDirMark() . wfMessage( 'articlefeedbackv5-contribs-feedback', $feedbackCentralPageTitle->getPrefixedDBkey(), $pageTitle->getFullText() )->parse();
 
 		// show user names for /newbies as there may be different users.
 		$userlink = '';
