e58baa24b65266d9b6f4a859362eca429b5e9a77
Matthias Mullie <git@mullie.eu>
2012-07-30 15:46:21 +0200

    Bug 38813 - My Contributions - Critical flood of slow queries that is causing a DB overload on en-wiki
    - added index for columns af_user_id & af_user_ip, who appear to be causing the problem
    - remove existing combined index af_user_id_user_ip_created & added index af_created - mysql can combine both new indexes to replace the removed one
    
    Change-Id: I89c9e24c7cc866ab57fa09812db0356f29d2b9b7

---
+++ sql/index_user_data.sql
@@ -0,0 +1,5 @@
+ALTER TABLE /*_*/aft_article_feedback
+DROP INDEX af_user_id_user_ip_created;
+
+CREATE INDEX /*i*/af_user_id_user_ip ON /*_*/aft_article_feedback (af_user_id, af_user_ip);
+CREATE INDEX /*i*/af_created ON /*_*/aft_article_feedback (af_created);
