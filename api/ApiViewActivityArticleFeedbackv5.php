02821974eaf57c71d8ef86cb78acf166c5d6f93c
Matthias Mullie <git@mullie.eu>
2012-08-03 16:29:51 +0200

    Bug 38816 - Feedback page - Clear all flags
    Change-Id: Id303499b8a649488efc1bd9e3dc02318d87be8e6

--- api/ApiViewActivityArticleFeedbackv5.php
+++ api/ApiViewActivityArticleFeedbackv5.php
@@ -54,6 +54,7 @@ class ApiViewActivityArticleFeedbackv5 extends ApiQueryBase {
 		$valid[] = 'undo-helpful';
 		$valid[] = 'unhelpful';
 		$valid[] = 'undo-unhelpful';
+		$valid[] = 'clear-flags';
 
 		// if we have hide permissions, we're allowed to see oversight-related
 		// actions as well
@@ -177,7 +178,7 @@ class ApiViewActivityArticleFeedbackv5 extends ApiQueryBase {
 
 			// determine the class a log entry is to get, based on whether it's a positive/negative
 			// taken against the feedback entry
-			$positiveActions = array( 'feature', 'resolve', 'unhidden', 'decline', 'unoversight', 'unflag', 'unrequest', 'helpful' );
+			$positiveActions = array( 'feature', 'resolve', 'unhidden', 'decline', 'unoversight', 'unflag', 'unrequest', 'helpful', 'clear-flags' );
 			$negativeActions = array( 'oversight', 'autohide', 'hidden', 'request', 'unfeature', 'unresolve', 'flag', 'autoflag', 'unhelpful' );
 			// neutral actions: undo-helpful, undo-unhelpful
 
