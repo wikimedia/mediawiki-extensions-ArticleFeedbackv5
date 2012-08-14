ca4bbdaf91eda4871275dfba960185f134f6e852
Matthias Mullie <git@mullie.eu>
2012-08-14 13:39:23 +0200

    Bug 38997 - Article Feedback Page - Remove survey button
    Change-Id: If3174a44e6b78fee2aa0ec0e60c650406a596f0e

--- SpecialArticleFeedbackv5.php
+++ SpecialArticleFeedbackv5.php
@@ -511,18 +511,6 @@ class SpecialArticleFeedbackv5 extends SpecialPage {
 			}
 		}
 
-		// Survey button
-		global $wgArticleFeedbackv5SpecialPageSurveyUrl;
-		if ( $wgArticleFeedbackv5SpecialPageSurveyUrl ) {
-			$out->addHTML(
-				Html::element( 'a', array(
-					'href'   => $wgArticleFeedbackv5SpecialPageSurveyUrl,
-					'target' => '_blank',
-					'class'  => 'articleFeedbackv5-survey-button',
-				), $this->msg( 'articlefeedbackv5-special-survey-button-text' )->text() )
-			);
-		}
-
 		// Link to add feedback (view article)
 		if ( $this->pageId ) {
 			$out->addHtml(
