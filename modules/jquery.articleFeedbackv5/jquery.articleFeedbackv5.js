17a065e8e9dc733adff20f75cfb9ba86b952cafe
Matthias Mullie <git@mullie.eu>
2012-08-28 17:17:10 +0200

    Bug 38591 - MediaWiki:Articlefeedbackv5-error-abuse prevents localisation
    Meanwhile refactored the displaying of errors:
    - it was bit of a mess that accepted it's data in way too many forms, streamlined now
    - we want to parse the messages in php where possible (better parser)
    
    Change-Id: Ia17ae8d6980ca30ac08e2e8c9d2355cbd1504664

--- modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.js
+++ modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.js
@@ -477,15 +477,12 @@
 			 * @return mixed  if ok, false; otherwise, an object as { 'field name' : 'message' }
 			 */
 			localValidation: function ( formdata ) {
-				var error = {};
-				var ok = true;
 				if ( ( !( 'comment' in formdata ) || formdata.comment == '' )
 					&& !( 'found' in formdata ) ) {
 					$.articleFeedbackv5.enableSubmission( false );
-					error.nofeedback = mw.msg( 'articlefeedbackv5-error-nofeedback' );
-					ok = false;
+					return mw.msg( 'articlefeedbackv5-error-nofeedback' );
 				}
-				return ok ? false : error;
+				return false;
 			}
 
 			// }}}
@@ -806,15 +803,12 @@
 			 * @return mixed  if ok, false; otherwise, an object as { 'field name' : 'message' }
 			 */
 			localValidation: function ( formdata ) {
-				var error = {};
-				var ok = true;
 				if ( ( !( 'comment' in formdata ) || formdata.comment == '' )
 					&& !( 'found' in formdata ) && !$( '#articleFeedbackv5-bucket6-toggle-yes').is( ':checked' ) ) {
 					$.articleFeedbackv5.enableSubmission( false );
-					error.nofeedback = mw.msg( 'articlefeedbackv5-error-nofeedback' );
-					ok = false;
+					return mw.msg( 'articlefeedbackv5-error-nofeedback' );
 				}
-				return ok ? false : error;
+				return false;
 			},
 
 			// }}}
@@ -2641,6 +2635,13 @@
 			'link': $.articleFeedbackv5.submittedLinkId
 		} );
 
+		// inject alternative language to get correct localized error messages for ?uselang=XY
+		if ( typeof mw.Uri().query.uselang !== 'undefined' ) {
+			data = $.extend( data, {
+				'uselang': mw.Uri().query.uselang
+			} );
+		}
+
 		// Track the submit click
 		$.articleFeedbackv5.trackClick( 'submit_attempt' );
 
@@ -2676,41 +2677,35 @@
 					// Track the success
 					$.articleFeedbackv5.trackClick( 'submit_success' );
 				} else {
+					var msg = mw.msg( 'articlefeedbackv5-error-unknown' );
 					var code = 'unknown';
-					var msg;
+
+					// fetch error information
 					if ( 'error' in data ) {
-						if ( typeof data.error === 'object' ) {
-							msg = data.error;
-							if ( 'code' in data.error ) {
-								code = data.error.code;
-							}
-						} else if ( 'articlefeedbackv5-error-abuse' == data.error ) {
-							msg = mw.msg( data.error );
-							code = 'afreject';
-						} else {
-							msg = mw.msg( data.error );
-						}
+						msg = data.error.info;
+						code = data.error.code;
 					} else if ( 'warning' in data ) {
-						// NB: Warnings come from the AbuseFilter and are
-						// already translated.
-						msg = data.warning;
-						code = 'afwarn';
-					} else {
-						msg = { info: mw.msg( 'articlefeedbackv5-error-unknown' ) };
+						msg = data.warning.info;
+						code = data.warning.code;
 					}
+
 					// Track the error
 					$.articleFeedbackv5.trackClick( 'submit_error_' + code );
+
 					// Set up error state
-					$.articleFeedbackv5.markFormErrors( { _api : msg } );
+					$.articleFeedbackv5.markFormErrors( msg );
 					$.articleFeedbackv5.unlockForm();
 				}
 			},
 			'error': function (xhr, tstatus, error) {
+				var msg = mw.msg( 'articlefeedbackv5-error-submit' );
+				var code = 'jquery';
+
 				// Track the error
-				$.articleFeedbackv5.trackClick( 'submit_error_jquery' );
+				$.articleFeedbackv5.trackClick( 'submit_error_' + code );
+
 				// Set up error state
-				var err = { _api: { info: mw.msg( 'articlefeedbackv5-error-submit' ) } };
-				$.articleFeedbackv5.markFormErrors( err );
+				$.articleFeedbackv5.markFormErrors( msg );
 				$.articleFeedbackv5.unlockForm();
 			}
 		} );
@@ -3055,27 +3050,12 @@
 	 *
 	 * @param object errors errors, indexed by field name
 	 */
-	$.articleFeedbackv5.markFormErrors = function ( errors ) {
-		if ( '_api' in errors ) {
-			if ( typeof errors._api == 'object' ) {
-				if ( 'info' in errors._api ) {
-					mw.log( mw.msg( errors._api.info ) );
-				} else {
-					mw.log( mw.msg( 'articlefeedbackv5-error-submit' ) );
-				}
-				$.articleFeedbackv5.markTopError( mw.msg( 'articlefeedbackv5-error-submit' ) );
-			} else {
-				mw.log( mw.msg( errors._api ) );
-				$.articleFeedbackv5.markTopError( errors._api );
-			}
-		} else {
-			mw.log( mw.msg( 'articlefeedbackv5-error-validation' ) );
-			if ( 'nofeedback' in errors ) {
-				$.articleFeedbackv5.markTopError( mw.msg( 'articlefeedbackv5-error-nofeedback' ) );
-			}
-		}
+	$.articleFeedbackv5.markFormErrors = function ( error ) {
+		mw.log( error );
+		$.articleFeedbackv5.markTopError( error );
+
 		if ( 'markFormErrors' in $.articleFeedbackv5.currentBucket() ) {
-			$.articleFeedbackv5.currentBucket().markFormErrors( errors );
+			$.articleFeedbackv5.currentBucket().markFormErrors( error );
 		}
 	};
 
