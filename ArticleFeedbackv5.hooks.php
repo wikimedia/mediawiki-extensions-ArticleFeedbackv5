a577665d7631932929ea36223191352f3e196cb9
Matthias Mullie <git@mullie.eu>
2012-08-28 17:17:10 +0200

    Bug 38591 - MediaWiki:Articlefeedbackv5-error-abuse prevents localisation
    Meanwhile refactored the displaying of errors:
    - it was bit of a mess that accepted it's data in way too many forms, streamlined now
    - we want to parse the messages in php where possible (better parser)
    
    Change-Id: Ia17ae8d6980ca30ac08e2e8c9d2355cbd1504664

--- ArticleFeedbackv5.hooks.php
+++ ArticleFeedbackv5.hooks.php
@@ -109,6 +109,7 @@ class ArticleFeedbackv5Hooks {
 				'articlefeedbackv5-error-submit',
 				'articlefeedbackv5-cta-thanks',
 				'articlefeedbackv5-error-abuse',
+				'articlefeedbackv5-error-abuse-link',
 				'articlefeedbackv5-error-throttled',
 				'articlefeedbackv5-cta-confirmation-message',
 				'articlefeedbackv5-cta1-confirmation-title',
@@ -241,6 +242,7 @@ class ArticleFeedbackv5Hooks {
 				'mediawiki.jqueryMsg',
 				'jquery.articleFeedbackv5.track',
 				'jquery.effects.highlight',
+				'mediawiki.Uri',
 			),
 		),
 		'jquery.articleFeedbackv5.special' => array(
