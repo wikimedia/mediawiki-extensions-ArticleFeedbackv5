9e41a891a641ebe27047fb46ca2caaded8bfd71a
Matthias Mullie <git@mullie.eu>
2012-09-09 23:34:56 +0200

    Refactor AFT backend
    * New AFT DB schema - once all experimental stuff is gone, AFT data is really simpel
    * Build solid feedback model for easier controller implementation (based on DataModel)
    * Remove & refactor lots and lots of pointless and/or abandonned code
    
    Change-Id: I5543ff04896d55bd73d9bc4cc5c9d36ed2580928

---
+++ ArticleFeedbackv5.backend.RDBStore.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * AFTv5-specific implementation to power DataModel, based on RDBStore.
+ *
+ * This class will connect to a single database setup with master/slaves
+ * architecture.
+ *
+ * @author     Matthias Mullie <mmullie@wikimedia.org>
+ * @version    $Id$
+ */
+class ArticleFeedbackv5BackendRDBStore extends DataModelBackendRDBStore {
+	/**
+	 * Get the amount of people who marked "yes" to the question if they
+	 * found what the were looking for.
+	 *
+	 * This is quite an expensive function, whose result should be cached.
+	 *
+	 * @param int[optional] The page id
+	 * @return int
+	 */
+	public function getCountFound( $pageId = null ) {
+		$storeGroup = RDBStoreGroup::singleton();
+		$store = $storeGroup->getForTable( $this->table );
+
+		// build where condition
+		$where = array();
+		$where['rating'] = 1;
+		if ( $pageId !== null) {
+			$where[$this->shardColumn] = $pageId;
+			$partitions = array( $store->getPartition( $this->table, $this->shardColumn, $pageId ) );
+		} else {
+			$partitions = $store->getAllPartitions( $this->table, $this->shardColumn );
+		}
+
+		$count = 0;
+		foreach ( $partitions as $partition ) {
+			$count += (int) $partition->selectField(
+				DB_SLAVE,
+				array( 'COUNT('.$this->idColumn.')' ),
+				$where,
+				__METHOD__,
+				array()
+			);
+		}
+
+		return $count;
+	}
+}
