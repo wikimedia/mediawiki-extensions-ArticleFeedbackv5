be464a447fb440e83bb3c7de9d2c1e4bf4c8a24c
Matthias Mullie <git@mullie.eu>
2012-11-26 17:01:57 +0100

    Feedback link on article pages
    * Add API endpoint to fetch amount of entries for a given page/filter
    * Code for above functionality was in use on special page already; moved
      that code to utils so both can access this and avoid code duplication
    * add Javascript ajax call to fetch the amount of feedback and display a
      link on article page
    * Update talk-page link to also only display if there is feedback
    
    Bug: 42057
    Change-Id: I03c815d8257aaa590ef639387c360d8356cd0d96

--- modules/ext.articleFeedbackv5/ext.articleFeedbackv5.talk.js
+++ modules/ext.articleFeedbackv5/ext.articleFeedbackv5.talk.js
@@ -4,37 +4,45 @@
 
 /*** Main entry point ***/
 jQuery( function( $ ) {
-
 	// Check if the talk page link can be shown
 	if ( mw.config.get( 'wgArticleFeedbackv5TalkPageLink' ) ) {
+		var api = new mw.Api();
+		api.get( {
+			'pageid': $.aftUtils.article().id,
+			'filter': 'featured',
+			'action': 'articlefeedbackv5-get-count',
+			'format': 'json'
+		} )
+		.done( function ( data ) {
+			if ( 'articlefeedbackv5-get-count' in data && 'count' in data['articlefeedbackv5-get-count'] ) {
+				var count = data['articlefeedbackv5-get-count']['count'];
 
-		// Build the url to the Special:ArticleFeedbackv5 page
-		var params = { ref: 'talk' };
-		var url = mw.config.get( 'wgArticleFeedbackv5SpecialUrl' ) + '/' +
-			mw.util.wikiUrlencode( mw.config.get( 'aftv5Article' ).title );
-		url = url + ( url.indexOf( '?' ) >= 0 ? '&' : '?' ) + $.param( params );
+				if ( count > 0 ) {
+					// Build the url to the Special:ArticleFeedbackv5 page
+					var params = { ref: 'talk', filter: 'featured' };
 
-		// Add the link to the feedback-page next to the title
-		var link = $( '<a id="articlefeedbackv5-talk-feedback-link"></a>' );
-		link
-			.text( mw.msg( 'articlefeedbackv5-talk-view-feedback' ) )
-			.html( link.html() + ' &raquo;' )
-			.attr( 'href', url )
-			.click( { trackingId: 'talk_page_view_feedback-button_click' }, $.aftTrack.trackEvent );
+					var url = mw.config.get( 'wgArticleFeedbackv5SpecialUrl' ) + '/' +
+						mw.util.wikiUrlencode( mw.config.get( 'aftv5Article' ).title );
+					url = url + ( url.indexOf( '?' ) >= 0 ? '&' : '?' ) + $.param( params );
 
-		$( '#firstHeading' ).append( link );
+					// Add the link to the feedback-page next to the title
+					var link = $( '<a id="articlefeedbackv5-talk-feedback-link"></a>' );
+					link
+						.text( mw.msg( 'articlefeedbackv5-talk-view-feedback', count ) )
+						.html( link.html() + ' &raquo;' )
+						.attr( 'href', url )
+						.click( { trackingId: 'talk_page_view_feedback-button_click' }, $.aftTrack.trackEvent );
+					$( '#firstHeading' ).append( link );
 
-		// Check if AFT is enabled
-		if ( $.aftUtils.verify( 'talk' ) ) {
-			// Initialize clicktracking
-			// NB: Using the talk page's namespace, title, and rev id, not
-			// the article's as in the front end tracking
-			$.aftTrack.init();
+					// Initialize clicktracking
+					// NB: Using the talk page's namespace, title, and rev id, not
+					// the article's as in the front end tracking
+					$.aftTrack.init();
 
-			// Track an impression
-			$.aftTrack.track( 'talk_page_view_feedback-impression' );
-		}
-
+					// Track an impression
+					$.aftTrack.track( 'talk_page_view_feedback-impression' );
+				}
+			}
+		} );
 	}
-
 } );
