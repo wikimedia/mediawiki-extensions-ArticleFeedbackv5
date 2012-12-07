8fff58295ed00831f3e3548979a80fc0b80e1c09
Matthias Mullie <git@mullie.eu>
2012-12-07 18:07:31 +0100

    Improve CT code
    - Implemented a way for Opera to trigger a real click
    - There's no way to trigger a real click on IE, but let's at least not stop UX behaviour and follow that clicked link ;)
    
    Change-Id: Ide033a452d44eafc7e0b11fcef854c55dcd1feb2

--- modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.track.js
+++ modules/jquery.articleFeedbackv5/jquery.articleFeedbackv5.track.js
@@ -143,14 +143,22 @@
 			return false;
 		}
 
-		/**
-		 * $.trackActionWithInfo ends with a $.post to submit the data to
-		 * ClickTracking API. We do not want any default behaviour to
-		 * interrupt that ajax call, so prevent any default behaviour (e.g.
-		 * redirect to a clicked link's href) until the call has completed
+		/*
+		 * IE does not appear to support a way to simulate a real event. Since we can
+		 * not resume an event nor trigger an exact new one, let's just not stop this
+		 * one (for IE) - I'm aware that this may result in aborted ClickTracking calls,
+		 * but correct UX behaviour is more important
 		 */
-		e.preventDefault();
-		e.stopPropagation();
+		if ( typeof e.target.fireEvent == 'undefined' ) {
+			/**
+			 * $.trackActionWithInfo ends with a $.post to submit the data to
+			 * ClickTracking API. We do not want any default behaviour to
+			 * interrupt that ajax call, so prevent any default behaviour (e.g.
+			 * redirect to a clicked link's href) until the call has completed
+			 */
+			e.preventDefault();
+			e.stopPropagation();
+		}
 
 		// submit call to ClickTracking API
 		$.aftTrack.track( e.data.trackingId )
@@ -165,10 +173,47 @@
 				 */
 				$( e.target ).off( e.type, $.aftTrack.trackEvent );
 
-				if ( typeof e.target.fireEvent == 'function' ) {
-					e.target.fireEvent( e.type );
-				} else {
-					e.target[ e.type ]();
+				if ( typeof e.target.dispatchEvent != 'undefined' ) {
+					/*
+					 * Multiple browsers support dispatchEvent, though with
+					 * different behaviour.
+					 * Opera (maybe other browsers as well) supports the general
+					 * "Events" as parameter for createEvent. Firefox (and maybe
+					 * others) need an exact e.g. "MouseEvent" for mouse events.
+					 * Opera will know what to do based on the first parameter
+					 * passed to initEvent (which is the actual event to perform),
+					 * Firefox however will ignore this.
+					 * IE9 does not appear to do much either way.
+					 * IE8 and lower do not even support dispatchEvent.
+					 * I do want to use the general "Events" though to keep this
+					 * code general-purpose.
+					 * To detect successful execution, we can check the event's
+					 * eventPhase property, which, if the event does not execute,
+					 * will not contain a valid value. If eventPhase contains value
+					 * 3, we know the event has been executed and bubbled up.
+					 *
+					 * @see https://developer.mozilla.org/en-US/docs/DOM/event.eventPhase
+					 */
+					var evt = document.createEvent( 'Events' );
+					evt.initEvent( e.type, e.bubbles, e.cancelable );
+					e.target.dispatchEvent( evt );
+				}
+				if ( typeof evt == 'undefined' || evt.eventPhase <= 2 ) {
+					if ( typeof e.target.fireEvent != 'undefined' ) {
+						/*
+						 * IE-specific; IE does not support <target>.<event>();
+						 * This will only fire attached events, but will not simulate
+						 * a "real event" (as in: triggering a click on a link will
+						 * not make the browser follow the link)
+						 */
+						e.target.fireEvent( 'on' + e.type );
+					} else {
+						/**
+						 * Firefox, Safari, Chrome & possibly others simulate a "real
+						 * event" from this (Opera & IE do not)
+						 */
+						e.target[ e.type ]();
+					}
 				}
 
 				$( e.target ).bind( e.type, e.data, $.aftTrack.trackEvent );
