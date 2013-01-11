c0141e91671365c16d0f8bd87fd4f5156d435472
Matthias Mullie <git@mullie.eu>
2013-01-11 14:39:40 +0100

    New actions, filters & UX
    * Change virtually all existing filters
    * Change actions "feature" to "useful" and "hide" to "inappropriate"
    * Introduce action "non-actionable"
    * Change the UX:
      * Now only 1 of the 4 editor tools (useful, resolved, non-actionable, inapropriate) can be performed at once
      * After having flagged on of those 4, "undo" button pops up as well as the details of the previous flag
      * Only after flagging as inappropriate do oversight-related tools pop up
      * Moderation actions are 1-click, with possibility for flyout window to leave comment afterwards
    
    See requirements at http://www.mediawiki.org/wiki/Article_feedback/Version_5/Feature_Requirements#Simpler_moderation_tools
    
    Change-Id: Ibf97654b0d9344fd89e0c8b3377fa821fdc9043f

---
+++ api/ApiAddFlagNoteArticleFeedbackv5.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * ApiAddFlagNoteArticleFeedbackv5 class
+ *
+ * @package    ArticleFeedback
+ * @subpackage Api
+ * @author     Matthias Mullie <mmullie@wikimedia.org>
+ */
+
+/**
+ * This class allows one to add a note describing activity, after the action
+ * has been performed already.
+ *
+ * @package    ArticleFeedback
+ * @subpackage Api
+ */
+class ApiAddFlagNoteArticleFeedbackv5 extends ApiBase {
+	public function __construct( $query, $moduleName ) {
+		parent::__construct( $query, $moduleName, '' );
+	}
+
+	/**
+	 * Execute the API call
+	 *
+	 * This single api call covers all cases where flags are being applied to
+	 * a piece of feedback
+	 */
+	public function execute() {
+		wfProfileIn( __METHOD__ );
+
+		global $wgUser;
+
+		// get important values from our parameters
+		$params     = $this->extractRequestParams();
+		$logId      = $params['logid'];
+		$notes      = $params['note'];
+
+		// update log entry in database
+		$dbw = wfGetDB( DB_MASTER );
+		$affected = $dbw->update(
+			'logging',
+			array( 'log_comment' => $notes ),
+			array(
+				'log_id' => $logId,
+				// failsafe, making sure this can't be gamed to add comments to other users' feedback
+				'log_user' => $wgUser->getId()
+			)
+		);
+
+		if ( $affected > 0 ) {
+			$results['result'] = 'Success';
+		} else {
+			$results['result'] = 'Error';
+		}
+
+		$this->getResult()->addValue(
+			null,
+			$this->getModuleName(),
+			$results
+		);
+
+		wfProfileOut( __METHOD__ );
+	}
+
+	/**
+	 * Gets the allowed parameters
+	 *
+	 * @return array the params info, indexed by allowed key
+	 */
+	public function getAllowedParams() {
+		return array(
+			'logid'     => array(
+				ApiBase::PARAM_REQUIRED => true,
+				ApiBase::PARAM_ISMULTI  => false,
+				ApiBase::PARAM_TYPE     => 'integer'
+			),
+			'note' => array(
+				ApiBase::PARAM_REQUIRED => true,
+				ApiBase::PARAM_ISMULTI  => false,
+				ApiBase::PARAM_TYPE     => 'string'
+			),
+		);
+	}
+
+	/**
+	 * Gets the parameter descriptions
+	 *
+	 * @return array the descriptions, indexed by allowed key
+	 */
+	public function getParamDescription() {
+		return array(
+			'logid'  => 'Log ID to update',
+			'note'   => 'Information on why the feedback activity occurred',
+		);
+	}
+
+	/**
+	 * Gets the api descriptions
+	 *
+	 * @return array the description as the first element in an array
+	 */
+	public function getDescription() {
+		return array(
+			'Add a note describing activity, after the action has been performed already.'
+		);
+	}
+
+	/**
+	 * Gets an example
+	 *
+	 * @return array the example as the first element in an array
+	 */
+	protected function getExamples() {
+		return array(
+			'api.php?action=articlefeedbackv5-add-flag-note&logid=1&note=text'
+		);
+	}
+
+	/**
+	 * Gets the version info
+	 *
+	 * @return string the SVN version info
+	 */
+	public function getVersion() {
+		return __CLASS__ . ': $Id$';
+	}
+
+	public function isWriteMode() { return true; }
+
+	public function mustBePosted() { return true; }
+
+}
