330ed36bbe1c66cf4d6779991869638f7fade92e
nischayn22 <nischayn22@gmail.com>
2013-03-07 01:45:02 +0530

    Change addHolder calls to addHolders as per change in AbuseFilter
    Ia4f6a56f tries to change addHolder to addHolders, this change is dependant on that.
    
    Change-Id: I1d89f2b39c9a2f68a2d0cc2858a3f73d6847ba76

--- ArticleFeedbackv5.utils.php
+++ ArticleFeedbackv5.utils.php
@@ -336,8 +336,7 @@ class ArticleFeedbackv5Utils {
 			// Set up variables
 			$title = Title::newFromID( $pageId );
 			$vars = new AbuseFilterVariableHolder;
-			$vars->addHolder( AbuseFilter::generateUserVars( $wgUser ) );
-			$vars->addHolder( AbuseFilter::generateTitleVars( $title , 'ARTICLE' ) );
+			$vars->addHolders( AbuseFilter::generateUserVars( $wgUser ), AbuseFilter::generateTitleVars( $title , 'ARTICLE' ) );
 			$vars->setVar( 'SUMMARY', 'Article Feedback 5' );
 			$vars->setVar( 'ACTION', 'feedback' );
 			$vars->setVar( 'new_wikitext', $value );
