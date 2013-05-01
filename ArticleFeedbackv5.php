aa17e2af308d1b344a1a6c249a0ae4166e530896
Matthias Mullie <git@mullie.eu>
2013-05-01 19:26:53 +0200

    Quickly enable/disable AFTv5
    Feature requirements: http://www.mediawiki.org/wiki/Article_feedback/Version_5/Feature_Requirements#Enable.2Fdisable_feedback_on_a_page
    
    TL;DR:
    * (if feedback disabled) "Request feedback" link in toolbox, on article page
    * (if feedback disabled) Yellow box on special page with button to re-enable
    * New "settings" in special page. When clicked, display link to enable/disable
    * Links to enable/disable go to:
      * (admins) page?action=protect, where detailed permissions can be set
      * (editors) api call; will set permissions to aft-reader (enable) or aft-editor (disable)
    * Refactored checks to verify if AFT should be displayed
      * Lottery now servers as the default value for page protection
        * No page protection set, lottery decides (lottery win = aft-reader, lottery lose = aft-noone)
        * Page protection set = overrides lottery (both enable or disable)
      * Unless disabled in page protection, whitelist categories will still enable AFTv5
      * Blacklist categories still override-disable all
    * Grouped tipsies code into 1 method & made a change to hide "settings" link if there's no content
    
    Change-Id: I7029a628ba724ba53f3aaebbf98e3d652c823464

--- ArticleFeedbackv5.php
+++ ArticleFeedbackv5.php
@@ -394,6 +394,7 @@ $wgExtensionCredits['other'][] = $wgArticleFeedbackv5Credits;
 $wgAutoloadClasses['ApiArticleFeedbackv5']              = __DIR__ . '/api/ApiArticleFeedbackv5.php';
 $wgAutoloadClasses['ApiViewRatingsArticleFeedbackv5']   = __DIR__ . '/api/ApiViewRatingsArticleFeedbackv5.php';
 $wgAutoloadClasses['ApiViewFeedbackArticleFeedbackv5']  = __DIR__ . '/api/ApiViewFeedbackArticleFeedbackv5.php';
+$wgAutoloadClasses['ApiSetStatusArticleFeedbackv5']     = __DIR__ . '/api/ApiSetStatusArticleFeedbackv5.php';
 $wgAutoloadClasses['ApiAddFlagNoteArticleFeedbackv5']   = __DIR__ . '/api/ApiAddFlagNoteArticleFeedbackv5.php';
 $wgAutoloadClasses['ApiFlagFeedbackArticleFeedbackv5']  = __DIR__ . '/api/ApiFlagFeedbackArticleFeedbackv5.php';
 $wgAutoloadClasses['ApiGetCountArticleFeedbackv5']      = __DIR__ . '/api/ApiGetCountArticleFeedbackv5.php';
@@ -436,6 +437,7 @@ $wgHooks['UserLoginComplete'][] = 'ArticleFeedbackv5Hooks::userLoginComplete';
 // API Registration
 $wgAPIListModules['articlefeedbackv5-view-feedback'] = 'ApiViewFeedbackArticleFeedbackv5';
 $wgAPIListModules['articlefeedbackv5-view-activity'] = 'ApiViewActivityArticleFeedbackv5';
+$wgAPIModules['articlefeedbackv5-set-status']        = 'ApiSetStatusArticleFeedbackv5';
 $wgAPIModules['articlefeedbackv5-add-flag-note']     = 'ApiAddFlagNoteArticleFeedbackv5';
 $wgAPIModules['articlefeedbackv5-flag-feedback']     = 'ApiFlagFeedbackArticleFeedbackv5';
 $wgAPIModules['articlefeedbackv5-get-count']         = 'ApiGetCountArticleFeedbackv5';
@@ -522,10 +524,16 @@ $wgResourceModules['jquery.articleFeedbackv5.utils'] = array(
 ) + $wgArticleFeedbackResourcePaths;
 $wgResourceModules['ext.articleFeedbackv5.startup'] = array(
 	'scripts' => 'ext.articleFeedbackv5/ext.articleFeedbackv5.startup.js',
+	'messages' => array(
+		'articlefeedbackv5-toolbox-enable',
+		'articlefeedbackv5-enabled-form-message',
+	),
 	'dependencies' => array(
 		'mediawiki.util',
 		'mediawiki.user',
 		'jquery.articleFeedbackv5.utils',
+		'mediawiki.jqueryMsg',
+		'mediawiki.api',
 	),
 ) + $wgArticleFeedbackResourcePaths;
 $wgResourceModules['ext.articleFeedbackv5'] = array(
@@ -537,8 +545,6 @@ $wgResourceModules['ext.articleFeedbackv5'] = array(
 		'articlefeedbackv5-fixedtab-linktext',
 		'articlefeedbackv5-bottomrighttab-linktext',
 		'articlefeedbackv5-section-linktext',
-		'articlefeedbackv5-toolbox-view',
-		'articlefeedbackv5-toolbox-add',
 		'articlefeedbackv5-article-view-feedback',
 	),
 	'dependencies' => array(
@@ -693,6 +699,8 @@ $wgResourceModules['jquery.articleFeedbackv5'] = array(
 		'articlefeedbackv5-disable-flyover-prefbutton',
 		'articlefeedbackv5-disable-preference',
 		'pipe-separator',
+		'articlefeedbackv5-toolbox-view',
+		'articlefeedbackv5-toolbox-add',
 	),
 	'dependencies' => array(
 		'jquery.appear',
@@ -719,6 +727,9 @@ $wgResourceModules['jquery.articleFeedbackv5.special'] = array(
 		'articlefeedbackv5-invalid-feedback-flag',
 		'articlefeedbackv5-invalid-feedback-state',
 		'articlefeedbackv5-feedback-reloaded-after-error',
+		'articlefeedbackv5-error-unknown',
+		'articlefeedbackv5-invalid-page-id',
+		'articlefeedbackv5-insufficient-permissions',
 
 		'articlefeedbackv5-comment-more',
 		'articlefeedbackv5-comment-less',
@@ -877,6 +888,9 @@ $wgResourceModules['jquery.articleFeedbackv5.special'] = array(
 		'articlefeedbackv5-activity-pane-header',
 
 		'articlefeedbackv5-beta-label',
+
+		'articlefeedbackv5-settings-status-enable',
+		'articlefeedbackv5-settings-status-disable',
 	),
 	'dependencies' => array(
 		'mediawiki.util',
