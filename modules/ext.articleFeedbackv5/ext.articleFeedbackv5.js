5f23bb57fe8adbe33a5fddff55e7e9ea87ab51d1
SamanthaNguyen <samanthanguyen1116@gmail.com>
2017-03-17 20:24:29 -0500

    Remove references to ClickTracking and EventLogging
    Bug: T160801
    Change-Id: I870e06f78355f60536ab67f2b22862ae3fba21d3

--- modules/ext.articleFeedbackv5/ext.articleFeedbackv5.js
+++ modules/ext.articleFeedbackv5/ext.articleFeedbackv5.js
@@ -43,8 +43,7 @@ if ( mw.config.get( 'wgArticleFeedbackv5ArticlePageLink' ) &&
 				// Add the link to the feedback-page next to the title
 				var $link = $( '<a id="articlefeedbackv5-article-feedback-link"></a>' )
 					.msg( 'articlefeedbackv5-article-view-feedback', count )
-					.attr( 'href', url )
-					.click( { trackingId: 'article_page_view_feedback-button_click' }, $.aftTrack.trackEvent );
+					.attr( 'href', url );
 
 				/*
 				 * Add the link next to #siteSub. Append to #siteSub node if
@@ -57,38 +56,8 @@ if ( mw.config.get( 'wgArticleFeedbackv5ArticlePageLink' ) &&
 					$link.insertAfter( '#siteSub' );
 				}
 
-				// Track an impression
-				$.aftTrack.init();
-				$.aftTrack.track( 'article_page_view_feedback-impression' );
 			}
 		}
-	} );
-}
-
-/* Add basic edit tracking, making use of $.aftTrack() already being set up */
-if ( $.aftTrack.clickTrackingOn ) {
-	var editEventBase = $.aftTrack.prefix( $aftDiv.articleFeedbackv5( 'experiment' ) );
-
-	$( 'span.editsection a, #ca-edit a, #ca-viewsource a' ).each( function() {
-		if ( $(this).is( '#ca-edit a' ) ) {
-			var event = 'edit_tab_link';
-		} else if ( $(this).is( '#ca-viewsource a' ) ) {
-			var event = 'view_source_tab_link';
-		} else {
-			var event = 'section_edit_link';
-		}
-
-		var href = $( this ).attr( 'href' );
-		var editUrl = href + ( href.indexOf( '?' ) >= 0 ? '&' : '?' ) + $.param( {
-			'articleFeedbackv5_click_tracking': 1,
-			'articleFeedbackv5_ct_cttoken': $.cookie( 'clicktracking-session' ),
-			'articleFeedbackv5_ct_usertoken': mw.user.id(),
-			'articleFeedbackv5_ct_event': editEventBase + '-' + event
-		} );
-
-		$(this)
-			.attr( 'href', editUrl )
-			.click( { trackingId: event + '-click' }, $.aftTrack.trackEvent );
 	} );
 }
 
