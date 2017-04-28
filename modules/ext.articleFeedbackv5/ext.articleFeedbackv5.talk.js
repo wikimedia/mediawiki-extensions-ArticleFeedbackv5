b07472745fcff8f3935f234dba845be0319eee67
Fomafix <fomafix@googlemail.com>
2017-04-28 06:19:21 +0200

    [WIP] Introduce eslint
    Change-Id: I2ce5dd126507c782cec798f43eefe892fdfebed9

--- modules/ext.articleFeedbackv5/ext.articleFeedbackv5.talk.js
+++ modules/ext.articleFeedbackv5/ext.articleFeedbackv5.talk.js
@@ -3,12 +3,16 @@
  */
 
 ( function ( $, mw ) {
-	/*** Main entry point ***/
+	/**
+	 * Main entry point
+	 */
 	$( function () {
+		var filter, api;
+
 		// Check if the talk page link can be shown
 		if ( mw.config.get( 'wgArticleFeedbackv5TalkPageLink' ) ) {
 
-			var filter = '*';
+			filter = '*';
 			/*
 			 * If AFT is disabled for this page, we'll only want to show the link if
 			 * there's leftover featured feedback.
@@ -17,25 +21,27 @@
 				filter = 'featured';
 			}
 
-			var api = new mw.Api();
+			api = new mw.Api();
 			api.get( {
 				pageid: $.aftUtils.article().id,
 				filter: filter,
 				action: 'articlefeedbackv5-get-count'
 			} )
 			.done( function ( data ) {
-				if ( 'articlefeedbackv5-get-count' in data && 'count' in data['articlefeedbackv5-get-count'] ) {
-					var count = data['articlefeedbackv5-get-count']['count'];
+				var count, url, $link;
+
+				if ( 'articlefeedbackv5-get-count' in data && 'count' in data[ 'articlefeedbackv5-get-count' ] ) {
+					count = data[ 'articlefeedbackv5-get-count' ].count;
 
 					if ( count > 0 ) {
 						// Build the url to the Special:ArticleFeedbackv5 page
-						var url =
+						url =
 							mw.config.get( 'wgArticleFeedbackv5SpecialUrl' ) + '/' +
 							mw.util.wikiUrlencode( mw.config.get( 'aftv5Article' ).title );
 						url += ( url.indexOf( '?' ) >= 0 ? '&' : '?' ) + $.param( { ref: 'talk', filter: 'featured' } );
 
 						// Add the link to the feedback-page next to the title
-						var $link = $( '<a id="articlefeedbackv5-talk-feedback-link"></a>' )
+						$link = $( '<a id="articlefeedbackv5-talk-feedback-link"></a>' )
 							.text( mw.msg( 'articlefeedbackv5-talk-view-feedback' ) )
 							.attr( 'href', url );
 
@@ -55,4 +61,4 @@
 			} );
 		}
 	} );
-} )( jQuery, mediaWiki );
+}( jQuery, mediaWiki ) );
