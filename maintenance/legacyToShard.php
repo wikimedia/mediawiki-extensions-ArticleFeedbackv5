wikimedia/mediawiki-extensions-ArticleFeedbackv5

commit 355bc30a5daa59e24526e004dfc6c4e37a6a3826

Refactor AFT backend
* New AFT DB schema - once all experimental stuff is gone, AFT data is really simple
* Build solid feedback model for easier controller implementation
* Remove & refactor lots and lots of pointless and/or abandonned code
* Moved actions-related code into activity class
* Removed "legacyCorrection" method that no longer applies

PS9: Spage's feedback
PS10: Lwelling's feedback
PS13: added unittests
PS15: Spage's feedback
PS18: Spage & Siebrand's feedback
PS21: Spage's feedback
PS23: Spage's feedback

Change-Id: Iebd886f35baf1f593975342c3c96df5233878e44

maintenance/legacyToShard.php
@@ -1,0 +1,316 @@
+<?php
+/**
+ * ArticleFeedbackv5_LegacyToShard class
+ *
+ * @package    ArticleFeedbackv5
+ * @author     Matthias Mullie <mmullie@wikimedia.org>
+ * @version    $Id$
+ */
+
+require_once ( getenv( 'MW_INSTALL_PATH' ) !== false
+	? getenv( 'MW_INSTALL_PATH' ) . '/maintenance/Maintenance.php'
+	: dirname( __FILE__ ) . '/../../../maintenance/Maintenance.php' );
+
+/**
+ * Move all relevant legacy data stored in aft_article_*
+ * tables to the aft_feedback table that will be sharded.
+ *
+ * @package    ArticleFeedbackv5
+ */
+class ArticleFeedbackv5_LegacyToShard extends Maintenance {
+	/**
+	 * Batch size
+	 *
+	 * @var int
+	 */
+	private $limit = 50;
+
+	/**
+	 * The number of entries completed
+	 *
+	 * @var int
+	 */
+	private $completeCount = 0;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Move all relevant legacy data stored in aft_article_* tables to the aft_feedback table that will be sharded.';
+	}
+
+	/**
+	 * Execute the script
+	 */
+	public function execute() {
+		$dbr = $this->getDB( DB_SLAVE );
+		if ( !$dbr->tableExists( 'aft_article_feedback' ) ) {
+			// not necessary to run, there is no source data
+			return;
+		} elseif ( !$dbr->tableExists( 'aft_article_feedback' ) ) {
+			// not possible to run, there is no target
+			$this->output( "Target table 'aft_feedback' does not exist.\n" );
+			return;
+		}
+
+		$this->output( "Moving legacy ArticleFeedbackv5 entries to sharded table.\n" );
+
+		$continue = 0;
+		while ( $continue !== null ) {
+			$continue = $this->moveBatch( $continue );
+			wfWaitForSlaves();
+
+			if ( $continue ) {
+				$this->output( "--moved to entry #$continue\n" );
+			}
+		}
+
+		$this->output( "Done. Moved " . $this->completeCount . " ArticleFeedbackv5 entries.\n" );
+	}
+
+	/**
+	 * Move a batch of AFT entries
+	 *
+	 * @param $continue int      [optional] the pull the next batch starting at
+	 *                           this aft_id
+	 */
+	public function moveBatch( $continue ) {
+		global
+			$wgArticleFeedbackv5HideAbuseThreshold,
+			$wgArticleFeedbackv5DisplayBuckets,
+			$wgArticleFeedbackv5CTABuckets,
+			$wgArticleFeedbackv5LinkBuckets,
+			$wgArticleFeedbackv5MaxCommentLength;
+
+		$dbr = $this->getDB( DB_SLAVE );
+
+		$rows = $dbr->select(
+			array(
+				'aft_article_feedback',
+				'rating' => 'aft_article_answer',
+				'answer' => 'aft_article_answer',
+				'long_answer' => 'aft_article_answer_text',
+				'page',
+			),
+			array(
+				'af_id',
+				'page_title',
+				'af_page_id', // page
+				'af_revision_id', // page_revision
+				'af_user_id', // user
+				'af_user_ip', // user_text
+				'af_user_anon_token', // user_token
+				'af_form_id', // form
+				'af_cta_id', // cta
+				'af_link_id', // link
+				'rating' => 'rating.aa_response_boolean', // rating
+				'comment' => "SUBSTR(IFNULL(long_answer.aat_response_text, answer.aa_response_text), 1, $wgArticleFeedbackv5MaxCommentLength)", // comment
+				'af_created' // timestamp
+			),
+			array(
+				'af_form_id' => array_keys( $wgArticleFeedbackv5DisplayBuckets['buckets'] ),
+				'af_cta_id' => array_keys( $wgArticleFeedbackv5CTABuckets['buckets'] ),
+				'af_link_id' => array_keys( $wgArticleFeedbackv5LinkBuckets['buckets'] ),
+				"af_id > $continue"
+			),
+			__METHOD__,
+			array(
+				'LIMIT'    => $this->limit,
+				'ORDER BY' => 'af_id'
+			),
+			array(
+				'rating' => array(
+					'INNER JOIN',
+					array(
+						'rating.aa_feedback_id = af_id',
+						'rating.aa_field_id' => array( '1', '16' )
+					)
+				),
+				'answer' => array(
+					'LEFT JOIN',
+					array(
+						'answer.aa_feedback_id = af_id',
+						'answer.aa_field_id' => array( '2', '17' )
+					)
+				),
+				'long_answer' => array(
+					'LEFT JOIN',
+					array( 'long_answer.aat_id = answer.aat_id' )
+				),
+				'page' => array(
+					'INNER JOIN',
+					array(
+						'page_id = af_page_id',
+					)
+				)
+			)
+		);
+
+		$continue = null;
+
+		foreach ( $rows as $row ) {
+			$continue = $row->af_id;
+
+			// build feedback entry
+			$feedback = new ArticleFeedbackv5Model;
+			$feedback->aft_id = $row->af_id;
+			$feedback->aft_page = $row->af_page_id;
+			$feedback->aft_page_revision = $row->af_revision_id;
+			$feedback->aft_user = $row->af_user_id;
+			$feedback->aft_user_text = $row->af_user_ip ? $row->af_user_ip : User::newFromId( $row->af_user_id )->getName();
+			$feedback->aft_user_token = $row->af_user_anon_token;
+			$feedback->aft_form = $row->af_form_id;
+			$feedback->aft_cta = $row->af_cta_id;
+			$feedback->aft_link = $row->af_link_id ? $row->af_link_id : 'X';
+			$feedback->aft_rating = $row->rating;
+			$feedback->aft_comment = isset( $row->comment ) ? $row->comment : '';
+			$feedback->aft_timestamp = $row->af_created;
+
+			// build username from user id
+			if ( $feedback->aft_user && !$feedback->aft_user_text ) {
+				$feedback->aft_user->text = $feedback->getUser()->getName();
+			}
+
+			$logging = $dbr->select(
+				array( 'logging' ),
+				array( 'log_action' ),
+				array(
+					'log_type' => array( 'articlefeedbackv5', 'suppress' ),
+					'log_action' => array(
+						'oversight', 'unoversight', 'decline', 'request', 'unrequest',
+						'hidden', 'hide', 'unhidden', 'unhide', 'flag', 'unflag', 'autoflag', 'autohide',
+						'feature', 'unfeature', 'resolve', 'unresolve', 'helpful',
+						'unhelpful', 'undo-helpful', 'undo-unhelpful', 'clear-flags'
+					),
+					'log_namespace' => NS_SPECIAL,
+					'log_page' => 0,
+					'log_title' => "ArticleFeedbackv5/$row->page_title/$row->af_id"
+				),
+				__METHOD__,
+				array(
+					'SORT BY' => 'log_id ASC'
+				)
+			);
+
+			foreach ( $logging as $log ) {
+				switch ( $log->log_action ) {
+					case 'oversight':
+						$feedback->aft_oversight = 1;
+						$feedback->aft_request = 0;
+						if ( !$feedback->isHidden() ) {
+							$feedback->aft_hide = 1;
+							$feedback->aft_autohide = 1;
+						}
+						break;
+					case 'unoversight':
+						$feedback->aft_oversight = 0;
+						$feedback->aft_request = 0;
+						if ( $feedback->aft_hide && $feedback->aft_autohide ) {
+							$feedback->aft_autohide = 0;
+							$feedback->aft_hide = 0;
+						}
+						break;
+					case 'decline':
+						$feedback->aft_decline = 1;
+						if ( $feedback->aft_hide && $feedback->aft_autohide ) {
+							$feedback->aft_hide = 0;
+							$feedback->aft_autohide = 0;
+						}
+						break;
+					case 'request':
+						$feedback->aft_request = 1;
+						$feedback->aft_decline = 0;
+						if ( !$feedback->isHidden() ) {
+							$feedback->aft_hide = 1;
+							$feedback->aft_autohide = 1;
+						}
+						break;
+					case 'unrequest':
+						$feedback->aft_request = 0;
+						if ( $feedback->aft_hide && $feedback->aft_autohide ) {
+							$feedback->aft_hide = 0;
+							$feedback->aft_autohide = 0;
+						}
+						break;
+					case 'hidden':
+					case 'hide':
+						$feedback->aft_hide = 1;
+						break;
+					case 'autohide':
+						$feedback->aft_hide = 1;
+						$feedback->aft_autohide = 1;
+						break;
+					case 'unhidden':
+					case 'unhide':
+						$feedback->aft_hide = 0;
+						$feedback->aft_autohide = 0;
+						if ( $feedback->aft_flag && $feedback->aft_autoflag ) {
+							$feedback->aft_autoflag = 0;
+							$feedback->aft_flag = 0;
+						}
+						break;
+					case 'flag':
+					case 'autoflag':
+						$feedback->{"aft_$log->log_action"}++;
+						if ( $feedback->aft_flag + $feedback->aft_autoflag > $wgArticleFeedbackv5HideAbuseThreshold && !$feedback->isHidden() ) {
+							$feedback->aft_hide = 1;
+							$feedback->aft_autohide = 1;
+						}
+						break;
+					case 'unflag':
+						if ( $feedback->aft_flag <= 0 ) {
+							$feedback->aft_autoflag = 0;
+						} else {
+							$feedback->aft_flag--;
+						}
+						if ( $feedback->aft_flag + $feedback->aft_autoflag < $wgArticleFeedbackv5HideAbuseThreshold && $feedback->aft_autohide ) {
+							$feedback->aft_autohide = 0;
+						}
+						break;
+					case 'feature':
+						$feedback->aft_feature = 1;
+						if ( $feedback->aft_flag && $feedback->aft_autoflag ) {
+							$feedback->aft_autoflag = 0;
+							$feedback->aft_flag = 0;
+						}
+						break;
+					case 'unfeature':
+						$feedback->aft_feature = 0;
+						break;
+					case 'resolve':
+						$feedback->aft_resolve = 1;
+						break;
+					case 'unresolve':
+						$feedback->aft_resolve = 0;
+						break;
+					case 'helpful':
+						$feedback->aft_helpful++;
+						break;
+					case 'unhelpful':
+						$feedback->aft_unhelpful++;
+						break;
+					case 'undo-helpful':
+						$feedback->aft_helpful--;
+						break;
+					case 'undo-unhelpful':
+						$feedback->aft_unhelpful--;
+						break;
+					case 'clear-flags':
+						$feedback->aft_autoflag = 0;
+						$feedback->aft_flag = 0;
+						break;
+				}
+			}
+
+			$feedback->insert();
+
+			$this->completeCount++;
+		}
+
+		return $continue;
+	}
+}
+
+$maintClass = "ArticleFeedbackv5_LegacyToShard";
+require_once( RUN_MAINTENANCE_IF_MAIN );

ArticleFeedbackv5.activity.php
@@ -1,0 +1,366 @@
+<?php
+/**
+ * This class provides some functionality to easily get feedback's activity.
+ * Because this data is less often requested & because we're dealing with
+ * the default MW logging table (which we can't "just change"), this is a
+ * completely different approach than ArticleFeedbackv5Model:
+ * - no datamodel/sharded data
+ * - few cache
+ * - no "lists"
+ * - queries, queries, queries
+ * - ...
+ *
+ * @package    ArticleFeedback
+ * @author     Matthias Mullie <mmullie@wikimedia.org>
+ * @version    $Id$
+ */
+class ArticleFeedbackv5Activity {
+	/**
+	 * The map of flags to permissions.
+	 * If an action is not mentioned here, it is not tied to specific permissions
+	 * and everyone is able to perform the action.
+	 *
+	 * @var array
+	 */
+	public static $actions = array(
+		'oversight' => array(
+			'permissions' => 'aft-oversighter',
+			'sentiment' => 'negative'
+		),
+		'unoversight' => array(
+			'permissions' => 'aft-oversighter',
+			'sentiment' => 'positive'
+		),
+		'decline' => array(
+			'permissions' => 'aft-oversighter',
+			'sentiment' => 'positive'
+		),
+		'request' => array(
+			'permissions' => 'aft-monitor',
+			'sentiment' => 'negative'
+		),
+		'unrequest' => array(
+			'permissions' => 'aft_monitor',
+			'sentiment' => 'positive'
+		),
+		'hide' => array(
+			'permissions' => 'aft-monitor',
+			'sentiment' => 'negative'
+		),
+		'unhide' => array(
+			'permissions' => 'aft-monitor',
+			'sentiment' => 'positive'
+		),
+		'autohide' => array(
+			'permissions' => 'aft-monitor',
+			'sentiment' => 'negative'
+		),
+		'flag' => array(
+			'permissions' => 'aft-reader',
+			'sentiment' => 'negative'
+		),
+		'unflag' => array(
+			'permissions' => 'aft-reader',
+			'sentiment' => 'positive'
+		),
+		'autoflag' => array(
+			'permissions' => 'aft-reader',
+			'sentiment' => 'negative'
+		),
+		'clear-flags' => array(
+			'permissions' => 'aft-monitor',
+			'sentiment' => 'positive'
+		),
+		'feature' => array(
+			'permissions' => 'aft-editor',
+			'sentiment' => 'positive'
+		),
+		'unfeature' => array(
+			'permissions' => 'aft-editor',
+			'sentiment' => 'negative'
+		),
+		'resolve' => array(
+			'permissions' => 'aft-editor',
+			'sentiment' => 'positive'
+		),
+		'unresolve' => array(
+			'permissions' => 'aft-editor',
+			'sentiment' => 'negative'
+		),
+		'helpful' => array(
+			'permissions' => 'aft-reader',
+			'sentiment' => 'positive'
+		),
+		'undo-helpful' => array(
+			'permissions' => 'aft-reader',
+			'sentiment' => 'neutral'
+		),
+		'unhelpful' => array(
+			'permissions' => 'aft-reader',
+			'sentiment' => 'negative'
+		),
+		'undo-unhelpful' => array(
+			'permissions' => 'aft-reader',
+			'sentiment' => 'neutral'
+		)
+	);
+
+	/**
+	 * Gets the last $limit of activity rows taken from the log table,
+	 * starting from point $continue, sorted by time - latest first
+	 *
+	 * @param ArticleFeedbackv5Model $feedback Model of the feedback item whose activity we're fetching
+	 * @param User[optional] $user User object who we're fetching activity for (to check permissions)
+	 * @param int[optional] $limit total limit number
+	 * @param string[optional] $continue used for offsets
+	 * @return array db record rows
+	 */
+	public static function getList( $feedback, $user = null, $limit = 25, $continue = null ) {
+		global $wgLogActionsHandlers;
+		$dbr = wfGetDB( DB_SLAVE );
+
+		$feedbackId = $feedback->aft_id;
+		$page = Title::newFromID( $feedback->aft_page );
+		if ( !$page ) {
+			throw new MWException( 'Page for feedback does not exist', 'invalidfeedbackid' );
+		}
+		$title = $page->getDBKey();
+
+		if ( !$user ) {
+			global $wgUser;
+			$user = $wgUser;
+		}
+
+		// can only see activity for actions that you have permissions to perform
+		$actions = array();
+		$permissions = $user->getRights();
+		foreach ( self::$actions as $action => $options ) {
+			if ( in_array( $options['permissions'], $permissions ) ) {
+				if ( isset( $wgLogActionsHandlers["suppress/$action"] ) ) {
+					$type = 'suppress';
+				} elseif ( isset( $wgLogActionsHandlers["articlefeedbackv5/$action"] ) ) {
+					$type = 'articlefeedbackv5';
+				} else {
+					continue;
+				}
+				$actions[] = 'log_type = '.$dbr->addQuotes( $type ).' AND log_action = '.$dbr->addQuotes( $action );
+			}
+		}
+
+		// nothing to get? return empty resultset
+		if ( !$actions ) {
+			return new FakeResultWrapper( array() );
+		}
+
+		$where[] = '('.implode( ') OR (', $actions ).')';
+		$where['log_namespace'] = NS_SPECIAL;
+		$where['log_title'] = "ArticleFeedbackv5/$title/$feedbackId";
+		$where = self::applyContinue( $continue, $where );
+
+		$activity = $dbr->select(
+			array( 'logging' ),
+			array(
+				'log_id',
+				'log_action',
+				'log_timestamp',
+				'log_user',
+				'log_user_text',
+				'log_title',
+				'log_comment'
+			),
+			$where,
+			__METHOD__,
+			array(
+				'LIMIT' => $limit + 1,
+				'ORDER BY' => 'log_timestamp DESC',
+				// Force the page_time index (on _namespace, _title, _timestamp)
+				// We don't expect many if any rows for Special:ArticleFeedbackv5/foo that
+				// don't match log_type='articlefeedbackv5' , so we can afford to have that
+				// clause be unindexed. The alternative is to have the log_type clause be indexed
+				// and the namespace/title clauses unindexed, that would be bad.
+				'USE INDEX' => 'page_time'
+			)
+		);
+
+		return $activity;
+	}
+
+	/**
+	 * Returns a timestamp/id tuple for subsequent request continuing from this record
+	 *
+	 * @param ResultWrapper $row
+	 * @return string
+	 */
+	public static function getContinue( $row ) {
+		$ts = wfTimestamp( TS_MW, $row->log_timestamp );
+		return "$ts|{$row->log_id}";
+	}
+
+	/**
+	 * Gets timestamp and id pair for continue
+	 *
+	 * @param string $continue
+	 * @param array $where
+	 * @return array
+	 */
+	protected static function applyContinue( $continue, $where ) {
+		if ( !$continue ) {
+			return $where;
+		}
+
+		$values = explode( '|', $continue, 3 );
+		if ( count( $values ) !== 2 ) {
+			throw new MWException( 'Invalid continue param. You should pass the value returned by the previous query', 'badcontinue' );
+		}
+
+		$db = wfGetDB( DB_SLAVE );
+		$ts = $db->addQuotes( $db->timestamp( $values[0] ) );
+		$id = intval( $values[1] );
+		$where[] = '(log_id = ' . $id . ' AND log_timestamp <= ' . $ts . ') OR log_timestamp < ' . $ts;
+
+		return $where;
+	}
+
+	/**
+	 * Check if a user has sufficient permissions to perform an action
+	 *
+	 * @param string $action
+	 * @param User[optional] $user
+	 * @return bool
+	 * @throws MWException
+	 */
+	public static function canPerformAction( $action, User $user = null ) {
+		if ( !$user ) {
+			global $wgUser;
+			$user = $wgUser;
+		}
+
+		if ( !isset( self::$actions[$action] ) ) {
+			throw new MWException( "Action '$action' does not exist." );
+		}
+
+		return $user->isAllowed( self::$actions[$action]['permissions'] ) && !$user->isBlocked();
+	}
+
+	/**
+	 * Get (and cache) the counts of activity (that are within the user's permissions)
+	 * that has been posted already
+	 *
+	 * @param ArticleFeedbackv5Model $feedback
+	 * @param User[optional] $user
+	 * @return int
+	 */
+	public static function getActivityCount( ArticleFeedbackv5Model $feedback, User $user = null ) {
+		global $wgArticleFeedbackv5Permissions, $wgMemc;
+		$total = 0;
+
+		if ( !$user ) {
+			global $wgUser;
+			$user = $wgUser;
+		}
+
+		if ( !$wgUser->isBlocked() ) {
+			foreach( $wgArticleFeedbackv5Permissions as $permission ) {
+				if ( $user->isAllowed( $permission ) ) {
+					// get count for this specific permission level from cache
+					$key = wfMemcKey( 'articlefeedbackv5', 'getActivityCount', $permission, $feedback->aft_id );
+					$count = $wgMemc->get( $key );
+
+					if ( $count === false ) {
+						$count = self::getActivityCountFromDB( $feedback, $permission );
+					}
+
+					/*
+					 * Save to or extend caching. Set a long expiration (because the
+					 * query-alternative is quite expensive) but not forever (once
+					 * feedback is dealt with, it won't be accessed again so there's
+					 * no point in cluttering memory)
+					 */
+					$wgMemc->set( $key, $count, 60 * 60 * 24 * 7 );
+
+					$total += $count;
+				}
+			}
+		}
+
+		return $total;
+	}
+
+	/**
+	 * Because fetching the amount of activity from db is quite expensive, this
+	 * method will just increment the data that is in cache already (instead of
+	 * purging the cache data to have it re-read from DB, which should be last-resort)
+	 *
+	 * @param int $feedbackId
+	 * @param string $action
+	 */
+	public static function incrementActivityCount( $feedbackId, $action ) {
+		global $wgMemc;
+
+		// get permission level that should be updated
+		$permission = ArticleFeedbackv5Activity::$actions[$action]['permissions'];
+
+		$key = wfMemcKey( 'articlefeedbackv5', 'getActivityCount', $permission, $feedbackId );
+		$count = $wgMemc->get( $key );
+
+		/*
+		 * if the data is not (yet) in cache, don't bother fetching it from db yet,
+		 * that'll happen in due time, when it's actually requested
+		 */
+		if ( $count !== false ) {
+			$wgMemc->set( $key, $count + 1, 60 * 60 * 24 * 7 );
+		}
+	}
+
+	/**
+	 * Get amount of activity for a certain feedback post for a certain permission level.
+	 * This should not be called directly, as it's a relatively expensive call; the result
+	 * should be cached (@see self::getActivityCount)
+	 *
+	 * @param ArticleFeedbackv5Model $feedback
+	 * @param string $permission
+	 * @return int
+	 * @throws MWException
+	 */
+	private static function getActivityCountFromDB( ArticleFeedbackv5Model $feedback, $permission ) {
+		global $wgLogActionsHandlers;
+		$dbr = wfGetDB( DB_SLAVE );
+
+		$feedbackId = $feedback->aft_id;
+		$page = Title::newFromID( $feedback->aft_page );
+		if ( !$page ) {
+			throw new MWException( 'Page for feedback does not exist', 'invalidfeedbackid' );
+		}
+		$title = $page->getDBKey();
+
+		// get action-specific where-clause for requested permission level
+		$actions = array();
+		foreach( self::$actions as $action => $options ) {
+			if ( $options['permissions'] == $permission ) {
+				if ( isset( $wgLogActionsHandlers["suppress/$action"] ) ) {
+					$type = 'suppress';
+				} elseif ( isset( $wgLogActionsHandlers["articlefeedbackv5/$action"] ) ) {
+					$type = 'articlefeedbackv5';
+				} else {
+					continue;
+				}
+				$actions[] = 'log_type = '.$dbr->addQuotes( $type ).' AND log_action = '.$dbr->addQuotes( $action );
+			}
+		}
+
+		if ( !$actions ) {
+			return 0;
+		}
+
+		$where[] = '('.implode( ') OR (', $actions ).')';
+		$where['log_namespace'] = NS_SPECIAL;
+		$where['log_title'] = "ArticleFeedbackv5/$title/$feedbackId";
+
+		return (int) $dbr->selectField(
+			'logging',
+			'COUNT(log_id)',
+			$where,
+			__METHOD__
+		);
+	}
+}

ArticleFeedbackv5.backend.LBFactory.php
@@ -1,0 +1,210 @@
+<?php
+/**
+ * AFTv5-specific implementation to power DataModel, allowing AFTv5 to be
+ * put on a separate cluster.
+ *
+ * This class will connect to a single database setup with master/slaves
+ * architecture.
+ *
+ * @author     Matthias Mullie <mmullie@wikimedia.org>
+ * @version    $Id$
+ */
+class ArticleFeedbackv5BackendLBFactory extends DataModelBackendLBFactory {
+	/**
+	 * Override getDB so that AFT's data can be on a separate cluster.
+	 *
+	 * @param $db Integer: index of the connection to get. May be DB_MASTER for the
+	 *            master (for write queries), DB_SLAVE for potentially lagged read
+	 *            queries, or an integer >= 0 for a particular server.
+	 * @param $groups Mixed: query groups. An array of group names that this query
+	 *                belongs to. May contain a single string if the query is only
+	 *                in one group.
+	 * @param $wiki String: the wiki ID, or false for the current wiki
+	 */
+	public function getDB( $db, $groups = array(), $wiki = false ) {
+		global $wgArticleFeedbackv5Cluster;
+
+		// connect to external, aft-specific, cluster
+		if ( $wgArticleFeedbackv5Cluster ) {
+			return wfGetLBFactory()->getExternalLB( $wgArticleFeedbackv5Cluster )->getConnection( $db, $groups, $wiki );
+		}
+
+		// plain old wfGetDB
+		return parent::getDB( $db, $groups, $wiki );
+	}
+
+	/**
+	 * Query to fetch entries from DB.
+	 *
+	 * @param mixed $id The id(s) to fetch, either a single id or an array of them
+	 * @param mixed $shard The corresponding shard value(s)
+	 * @return ResultWrapper
+	 */
+	public function get( $id = null, $shard = null ) {
+		$ids = null;
+		if ( $id != null ) {
+			$ids = (array) $id;
+		}
+
+		$ids = array_map( array( $this, 'standardizeId' ), $ids );
+
+		return parent::get( $ids, $shard );
+	}
+
+	/**
+	 * Update entry.
+	 *
+	 * @param DataModel $entry
+	 * @return int
+	 */
+	public function update( DataModel $entry ) {
+		/*
+		 * The clone will make sure it's no longer the same object referenced
+		 * inside DataModel.
+		 */
+		$entry = clone( $entry );
+		$entry->{$this->idColumn} = $this->standardizeId( $entry->{$this->idColumn} );
+
+		return parent::update( $entry );
+	}
+
+	/**
+	 * Delete entry.
+	 *
+	 * @param DataModel $entry
+	 * @return int
+	 */
+	public function delete( DataModel $entry ) {
+		/*
+		 * The clone will make sure it's no longer the same object referenced
+		 * inside DataModel.
+		 */
+		$entry = clone( $entry );
+		$entry->{$this->idColumn} = $this->standardizeId( $entry->{$this->idColumn} );
+
+		return parent::delete( $entry );
+	}
+
+	/**
+	 * Evaluate an entry to possible conditions.
+	 *
+	 * Before updating data, DataModel will want to re-evaluate en entry to
+	 * all possible conditions, to know which caches need to be purged/updated.
+	 *
+	 * @param DataModel $entry
+	 * @return ResultWrapper
+	 */
+	public function evaluateConditions( DataModel $entry ) {
+		/*
+		 * The clone will make sure it's no longer the same object referenced
+		 * inside DataModel.
+		 */
+		$entry = clone( $entry );
+		$entry->{$this->idColumn} = $this->standardizeId( $entry->{$this->idColumn} );
+
+		return parent::evaluateConditions( $entry );
+	}
+
+	/**
+	 * For a given list name, this will fetch the list's conditions.
+	 *
+	 * @param string $name The list name (see <datamodel>::$lists)
+	 * @return string
+	 */
+	public function getConditions( $name ) {
+		$class = $this->datamodel;
+
+		$conditions = array();
+		if ( isset( $class::$lists[$name]['conditions'] ) ) {
+			$conditions = $class::$lists[$name]['conditions'];
+		}
+
+		if ( empty( $conditions ) ) {
+			$conditions = array();
+		}
+
+		return $conditions;
+	}
+
+	/**
+	 * Get the amount of people who marked "yes" to the question if they
+	 * found what the were looking for.
+	 *
+	 * This is quite an expensive function, whose result should be cached.
+	 *
+	 * @param int[optional] The page id
+	 * @return int
+	 */
+	public function getCountFound( $pageId = null ) {
+		// build where condition
+		$where = array();
+		$where['aft_rating'] = 1;
+		if ( $pageId !== null) {
+			$where['aft_page'] = $pageId;
+		}
+
+		return (int) $this->getDB( DB_SLAVE )->selectField(
+			$this->table,
+			array( 'COUNT(*)' ),
+			$where,
+			__METHOD__,
+			array()
+		);
+	}
+
+	/**
+	 * Get a user's AFT-contributions to add to the My Contributions special page
+	 *
+	 * @param ContribsPager $pager object hooked into
+	 * @param string $offset index offset, inclusive
+	 * @param int $limit exact query limit
+	 * @param bool $descending query direction, false for ascending, true for descending
+	 * @param array $userIds array of user_ids whose data is to be selected
+	 * @return ResultWrapper
+	 */
+	public function getContributionsData( $pager, $offset, $limit, $descending, $userIds = array() ) {
+		$tables[] = 'aft_feedback';
+
+		$fields[] = 'aft_id';
+		$fields[] = 'aft_page';
+		$fields[] = '"AFT" AS aft_contribution';
+		$fields[] = 'aft_timestamp AS ' . $pager->getIndexField(); // used for navbar
+
+		if ( $pager->contribs == 'newbie' ) {
+			$conds['aft_user'] = $userIds;
+		} else {
+			$uid = User::idFromName( $pager->target );
+			if ( $uid ) {
+				$conds['aft_user'] = $uid;
+			} else {
+				$conds['aft_user'] = 0;
+				$conds['aft_user_text'] = $pager->target;
+			}
+		}
+
+		if ( $offset ) {
+			$operator = $descending ? '>' : '<';
+			$conds[] = "aft_timestamp $operator " . $pager->getDatabase()->addQuotes( $offset );
+		}
+
+		$order = $descending ? 'ASC' : 'DESC'; // something's wrong with $descending - see logic applied in includes/Pager.php
+		$options['ORDER BY'] = array( $pager->getIndexField() . " $order" );
+		$options['LIMIT'] = $limit;
+
+		return $this->getDB( DB_SLAVE )->select( $tables, $fields, $conds, __METHOD__, $options, array() );
+	}
+
+	/**
+	 * ID is saved as binary(32), but all older id values (of feedback
+	 * prior to major code refactor, that have been migrated) will
+	 * remain unchanged, which will result in MySQL padding them to 32
+	 * length with null-bytes. Make sure out values are re-padded to
+	 * 32 characters before looking for them in the database.
+	 *
+	 * @param string $id The id
+	 * @return string
+	 */
+	protected function standardizeId( $id ) {
+		return str_pad( $id, 32, chr( 0 ) );
+	}
+}

commit d2e0d083576d1386a3ee62a5d0d282faa4e90303

Fix table name check in legacyToShard.php maintenance script
Apparently this typo had been here ever since the script was first introduced in 355bc30a5daa59e24526e004dfc6c4e37a6a3826 (March 2013).

Change-Id: Ib952ee451eae73a617b661e37ea15a951e3ad589

maintenance/legacyToShard.php
@@ -52,7 +52,7 @@ class ArticleFeedbackv5_LegacyToShard extends LoggedUpdateMaintenance {
 		if ( !$dbr->tableExists( 'aft_article_feedback' ) ) {
 			// not necessary to run, there is no source data
 			return true;
-		} elseif ( !$dbr->tableExists( 'aft_article_feedback' ) ) {
+		} elseif ( !$dbr->tableExists( 'aft_feedback' ) ) {
 			// not possible to run, there is no target
 			$this->output( "Target table 'aft_feedback' does not exist.\n" );
 			return false;